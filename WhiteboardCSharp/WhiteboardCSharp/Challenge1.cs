0e68958ec8a3b44e88870069572d9e00fae2696a
mattjohannsen <majohann@hotmail.com>
2020-05-20 17:30:11 -0500

    The correct numbers are being output correctly that add up to the target

--- WhiteboardCSharp/WhiteboardCSharp/Challenge1.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge1.cs
@@ -12,7 +12,31 @@ namespace WhiteboardCSharp
         public int[] RunChallenge(int[] inputArray, int inputTarget)
         {
             int[] returnArray = new int[2];
-
+            int firstNumber;
+            int secondNumber;
+            //Loop through the inputArray for first value
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine(inputArray[i]);
+                //Loop through secondArray for the second value
+                for (int j = 0; j < (inputArray.Length); j++)
+                {
+                    if (i==j)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(inputArray[i] + inputArray[j]);
+                        if(inputArray[i] + inputArray[j] == inputTarget)
+                        {
+                            returnArray[0] = inputArray[i];
+                            returnArray[1] = inputArray[j];
+                            Console.WriteLine(i + " " + j);
+                        }
+                    }
+                }
+            }
 
             return returnArray;
         }
--- WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
@@ -14,7 +14,13 @@ namespace WhiteboardCSharp
             int[] challengeArray = { 5, 17, 77, 50 };
             int target = 55;
             Challenge1 challenge1 = new Challenge1();
-            challenge1.RunChallenge(challengeArray, target);
+            int[] challenge1Array = challenge1.RunChallenge(challengeArray, target);
+            Console.Write($"The two numbers that add up to {target} are: ");
+            for (int i = 0; i < challenge1Array.Length; i++)
+            {
+                Console.Write(challenge1Array[i] + " ");
+            }
+            
         }
     }
 }
