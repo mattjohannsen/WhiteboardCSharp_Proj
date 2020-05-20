48b5af95a2116db019f2d961bf94c042a7746c65
mattjohannsen <majohann@hotmail.com>
2020-05-20 11:40:07 -0500

    Initial upload for WhiteboardCSharp

---
+++ WhiteboardCSharp/WhiteboardCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Whiteboard whiteboard = new Whiteboard();
+            whiteboard.ShowWhiteboard();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Whiteboard
+    {
+        public void ShowWhiteboard()
+        {
+            Console.WriteLine("\n\n       Welcome to Matt's C# Whiteboard!");
+        }
+    }
+}
