683fd32725c4744629cb36bdd175796b526451b7
mattjohannsen <majohann@hotmail.com>
2020-05-20 17:15:13 -0500

    Added Whiteboard and Challenge1 class

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge1
+    {
+
+        public int[] RunChallenge(int[] inputArray, int inputTarget)
+        {
+            int[] returnArray = new int[2];
+
+
+            return returnArray;
+        }
+    }
+}
--- WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
@@ -11,6 +11,10 @@ namespace WhiteboardCSharp
         public void ShowWhiteboard()
         {
             Console.WriteLine("\n\n       Welcome to Matt's C# Whiteboard!");
+            int[] challengeArray = { 5, 17, 77, 50 };
+            int target = 55;
+            Challenge1 challenge1 = new Challenge1();
+            challenge1.RunChallenge(challengeArray, target);
         }
     }
 }
