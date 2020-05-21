d66455ab8684c836155d47374cc18420b95b9814
mattjohannsen <majohann@hotmail.com>
2020-05-21 08:31:05 -0500

    Moved the logic for Challenge1 out of the Whiteboard class and into Challenge1 class

--- WhiteboardCSharp/WhiteboardCSharp/Challenge1.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge1.cs
@@ -8,12 +8,32 @@ namespace WhiteboardCSharp
 {
     public class Challenge1
     {
+        int[] challengeArray = { 5, 17, 77, 50 };
+        int target;
+        int[] indecesArray;
 
-        public int[] RunChallenge(int[] inputArray, int inputTarget)
+        public Challenge1()
+        {
+            target = 55;
+        }
+        public void RunChallenge()
+        {
+            ChallengeDescription();
+            indecesArray = GetIndeces(challengeArray, target);
+            DisplayAnswer(indecesArray);
+
+        }
+
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("\n       Problem #1");
+            Console.WriteLine("       Given the following array of values: { 5, 17, 77, 50 }");
+        }
+
+        public int[] GetIndeces(int[] inputArray, int inputTarget)
         {
             int[] returnArray = new int[2];
-            int firstNumber;
-            int secondNumber;
+
             //Loop through the inputArray for first value
             for (int i = 0; i < inputArray.Length; i++)
             {
@@ -21,14 +41,14 @@ namespace WhiteboardCSharp
                 //Loop through secondArray for the second value
                 for (int j = 0; j < (inputArray.Length); j++)
                 {
-                    if (i==j)
+                    if (i == j)
                     {
                         continue;
                     }
                     else
                     {
                         //Console.WriteLine(inputArray[i] + inputArray[j]);
-                        if(inputArray[i] + inputArray[j] == inputTarget)
+                        if (inputArray[i] + inputArray[j] == inputTarget)
                         {
                             returnArray[0] = i;
                             returnArray[1] = j;
@@ -37,8 +57,15 @@ namespace WhiteboardCSharp
                     }
                 }
             }
-
             return returnArray;
+        }
+        public void DisplayAnswer(int[] inputArray)
+        {
+            Console.Write($"       The indeces of the two numbers that add up to {target} are: ");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.Write(inputArray[i] + " ");
+            }
         }
     }
 }
