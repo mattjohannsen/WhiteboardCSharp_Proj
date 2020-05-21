d3f11360f888b2532c0a2b64712e7fc2c1256ab2
mattjohannsen <majohann@hotmail.com>
2020-05-21 08:58:49 -0500

    Completed ChallengeDescription method in Challenge2, and added coding comments about implementation strategy for Challenge2

--- WhiteboardCSharp/WhiteboardCSharp/Challenge2.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge2.cs
@@ -8,17 +8,32 @@ namespace WhiteboardCSharp
 {
     class Challenge2
     {
-
+        string wordToCheck;
 
         public void RunChallenge()
         {
+            ChallengeDescription();
+        }
+
+        public void ChallengeDescription()
+        {
             Console.WriteLine("\n\n       Problem #2");
+            Console.WriteLine("       Palindrome is a word, phrase, or sequence that reads the same backward as forward i.e.");
+            Console.WriteLine("       madam. Write code that takes a user input and checks to see if it is a Palindrome and reports");
+            Console.WriteLine("       the result. You must handle spaces.");
 
         }
     }
 
 
-    //2.	Palindrome is a word, phrase, or sequence that reads the same backward as forward i.e.
-    //    madam.Write code that takes a user input and checks to see if it is a Palindrome and reports 
-    //    the result. You must handle spaces. 
+    //    Step 1) Capture user input
+    //    Step 2) Get word length
+    //    Step 3) Divide by 2
+    //    Step 4) If odd...
+    //            a) Since integer rounds down the result will be the middle index and also the number of times we need to run a check on the characters
+    //            b) Check if the characters match on indeces +1 and -1.
+    //    Step 5) If even...
+    //            a) Compare the characters at the (length divided by 2)index and (the length divided by 2 -1)index
+    //            b) Run this check until false or length/2 times.
+    //    Step 6) If everthing matches... it is a palindrome
 }
