26824404ff5e9671a5b6eca7bb5221531c40e185
mattjohannsen <majohann@hotmail.com>
2020-05-25 11:12:44 -0500

    Challenge5 successfully accepts an input string, turns it into an integer list, gets the lowest and highest value and returns them as one string

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge5.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge5
+    {
+        public void RunChallenge()
+        {
+            string startingString = "3 9 0 1 4 8 10 2";
+
+            Console.WriteLine("       This is challenge 5");
+            Console.WriteLine(GetSolution(startingString));
+
+        }
+
+        public string GetSolution(string inputString)
+        {
+            string solutionString = "";
+            int solutionInt;
+            List<string> startingStringList = inputString.Split(' ').ToList();
+            List<int> startingIntList = startingStringList.Select(int.Parse).ToList();
+            List<int> solutionList = new List<int>();
+            List<string> solutionStringList = new List<string>();
+
+            startingIntList.Sort();
+            solutionList.Add(startingIntList[0]);
+            solutionList.Add(startingIntList[startingIntList.Count - 1]);
+            for (int i = 0; i < solutionList.Count; i++)
+            {
+                solutionStringList.Add(solutionList[i].ToString());
+            }
+            for (int i = 0; i < solutionStringList.Count; i++)
+            {
+                Console.WriteLine(solutionStringList[i]);
+            }
+            solutionString = solutionStringList.ToString();
+            solutionString = string.Join(" ", solutionStringList.ToArray());
+            //solutionString = string.Join(";", myList.ToArray());
+            return solutionString;
+            //List<int> startingIntList = 
+        }
+    }
+}
+//5.	Create a method that accepts a string of space separated numbers and returns the highest and lowest number as a string
+//a.Use case: “3 9 0 1 4 8 10 2”  “0 10”
+
+//    Steps
+//    Create a string variable equal to “3 9 0 1 4 8 10 2”.
+//    Create a list variable
+//    Create a solution list variable
+//    Create a solution string variable
+//    Split that string into pieces and convert it ToList and make it equal the list variable
+//    Sort the list
+//    The lowest number will be at the 0 index of the list variable
+//        Add this to the Solution List at index 0
+//    The highest number will be at the Count -1 Index of the list variable.
+//        Add this to the Solution List at index 1
+//    Convert the Solution List ToString and make this value equal the Solution String
+//    Return the Solution String.
+
--- WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Whiteboard.cs
@@ -13,16 +13,16 @@ namespace WhiteboardCSharp
             //Challenge1 challenge1 = new Challenge1();
             //Challenge2 challenge2 = new Challenge2();
             //Challenge3 challenge3 = new Challenge3();
-            
-
-            Challenge4 challenge4 = new Challenge4();
+            //Challenge4 challenge4 = new Challenge4();
+            Challenge5 challenge5 = new Challenge5();
             
             Console.WriteLine("\n\n       Welcome to Matt's C# Whiteboard!");
-            
+
+            challenge5.RunChallenge();
             //challenge1.RunChallenge();
             //challenge2.RunChallenge();
             //challenge3.RunChallenge();
-            challenge4.RunChallenge();
+            //challenge4.RunChallenge();
 
         }
     }
