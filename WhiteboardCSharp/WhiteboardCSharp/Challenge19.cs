92386b1ba2c3f4756b011b87378f86eba67672d6
mattjohannsen <majohann@hotmail.com>
2020-06-22 21:04:53 -0500

    Added ChallengeDescription, ShowAnswer methods and condensed code

--- WhiteboardCSharp/WhiteboardCSharp/Challenge19.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge19.cs
@@ -10,34 +10,49 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 19");
-            string [] originalArray = { "Tomato", "Potato", "Pair" };
-            string [] theAnswer = IsFourLetters(originalArray);
-            for (int i = 0; i < theAnswer.Length; i++)
-            {
-                Console.WriteLine(theAnswer[i]);
-            }
+            ChallengeDesription();
+            string [] ArrayOne = { "Tomato", "Potato", "Pair" };
+            string [] theAnswer = IsFourLetters(ArrayOne);
+            ShowAnswer(theAnswer);
+            string[] ArrayTwo = { "Kangaroo", "Bear", "Fox" };
+            ShowAnswer(IsFourLetters(ArrayTwo));
+            string[] ArrayThree = { "Ryan", "Kieran", "Jason", "Matt" };
+            ShowAnswer(IsFourLetters(ArrayThree));
         }
-
         public string [] IsFourLetters(string [] inputArray)
         {
             List<string> returnList = new List<string>();
             for (int i = 0; i < inputArray.Length; i++)
             {
-                string tempString = inputArray[i];
-                List<char> tempList = tempString.ToList();
-                int listCount = tempList.Count;
-                if (listCount == 4)
+                List<char> stringAsList = inputArray[i].ToList();
+                if (stringAsList.Count == 4)
                 {
-                    returnList.Add(tempString);
+                    returnList.Add(inputArray[i]);
                 }
             }
-            string [] returnArray = returnList.ToArray();
-            return returnArray;
+            return returnList.ToArray();
+        }
+        public void ShowAnswer(string [] inputArray)
+        {
+            Console.Write("\n       ");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.Write(inputArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public void ChallengeDesription()
+        {
+            Console.WriteLine("       Challenge 19: Return the Four Letter Strings");
+            Console.WriteLine("       Create a function that takes an array of strings and returns the words that are exactly four letters.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       IsFourLetters('Tomato', 'Potato', 'Pair') --> ['Pair']");
+            Console.WriteLine("       IsFourLetters('Kangaroo', 'Bear', 'Fox') --> ['Bear']");
+            Console.WriteLine("       IsFourLetters('Ryan', 'Kieran', 'Jason', 'Matt') --> ['Ryan', 'Matt']");
         }
     }
 }
-//Return the Four Letter Strings
+//Return the Four Letter Strings https://edabit.com/challenge/W4x4o2M7ny6Cqkfhn
 //Create a function that takes an array of strings and returns the words that are exactly four letters.
 //Examples
 //IsFourLetters(["Tomato", "Potato", "Pair"]) --> ["Pair"]
