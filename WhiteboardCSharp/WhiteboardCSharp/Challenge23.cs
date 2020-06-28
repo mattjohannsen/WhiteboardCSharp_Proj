f425e698e975f62dc55303c7473fdfd555814bc4
mattjohannsen <majohann@hotmail.com>
2020-06-28 10:38:16 -0500

    Added ChallengeDescription and ShowAnswer methods to Challenge23

--- WhiteboardCSharp/WhiteboardCSharp/Challenge23.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge23.cs
@@ -10,14 +10,16 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 23");
+            ChallengeDescription();
             int num = 7;
             int length = 5;
-            int[] theAnswer = ArrayOfMultiples(num, length);
-            for (int i = 0; i < theAnswer.Length; i++)
-            {
-                Console.WriteLine(theAnswer[i]);
-            }
+            ShowAnswer(ArrayOfMultiples(num, length));
+            num = 12;
+            length = 10;
+            ShowAnswer(ArrayOfMultiples(num, length));
+            num = 17;
+            length = 6;
+            ShowAnswer(ArrayOfMultiples(num, length));
         }
 
         public int[] ArrayOfMultiples(int num, int length)
@@ -29,14 +31,32 @@ namespace WhiteboardCSharp
             }
             return solutionList.ToArray();
         }
+
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 23: Array of Multiples");
+            Console.WriteLine("       Create a function that takes two numbers as arguments(num, length) and");
+            Console.WriteLine("       returns an array of multiples of num up to length.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       ArrayOfMultiples(7, 5) --> [7, 14, 21, 28, 35]");
+            Console.WriteLine("       ArrayOfMultiples(12, 10) --> [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]");
+            Console.WriteLine("       ArrayOfMultiples(17, 6) --> [17, 34, 51, 68, 85, 102]");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       Notice that num is also included in the returned array.\n");
+        }
+
+        public void ShowAnswer(int[] inputArray)
+        {
+           Console.WriteLine($"       [{String.Join(", ", inputArray)}]");
+        }
     }
 }
-//Array of Multiples
+//Array of Multiples https://edabit.com/challenge/2QvnWexKoLfcJkSsc
 //Create a function that takes two numbers as arguments(num, length) and returns an array of multiples of num up to length.
 //Examples
-//ArrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
-//ArrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
-//ArrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+//ArrayOfMultiples(7, 5) --> [7, 14, 21, 28, 35]
+//ArrayOfMultiples(12, 10) --> [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+//ArrayOfMultiples(17, 6) --> [17, 34, 51, 68, 85, 102]
 //Notes
 //Notice that num is also included in the returned array.
 // Steps
