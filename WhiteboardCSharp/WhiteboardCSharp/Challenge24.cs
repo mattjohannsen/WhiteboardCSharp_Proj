d8d6c2336ff0248c381d118664394dc671cbdb22
mattjohannsen <majohann@hotmail.com>
2020-06-30 21:30:21 -0500

    Challenge 24: Added ChallengeDescription and condensed code

--- WhiteboardCSharp/WhiteboardCSharp/Challenge24.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge24.cs
@@ -10,14 +10,17 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 24");
+            ChallengeDescription();
             int originalNumber = 0;
-            Console.WriteLine(CountOnes(originalNumber));
+            Console.WriteLine($"       Original number: {originalNumber} #1's: {CountOnes(originalNumber)}");
+            originalNumber = 100;
+            Console.WriteLine($"       Original number: {originalNumber} #1's: {CountOnes(originalNumber)}");
+            originalNumber = 999;
+            Console.WriteLine($"       Original number: {originalNumber} #1's: {CountOnes(originalNumber)}");
         }
         public int CountOnes(int inputNumber)
         {
-            string binaryNumber = Convert.ToString(inputNumber, 2);
-            List<char> binaryList = binaryNumber.ToList();
+            List<char> binaryList = Convert.ToString(inputNumber, 2).ToList();
             int binaryCount = 0;
             for (int i = 0; i < binaryList.Count; i++)
             {
@@ -26,8 +29,20 @@ namespace WhiteboardCSharp
                     binaryCount++;
                 }
             }
-
             return binaryCount;
+        }
+
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 24: Count Ones in Binary Representation of Integer");
+            Console.WriteLine("       Count the amount of ones in the binary representation of an integer.");
+            Console.WriteLine("       So for example, since 12 is '1100' in binary, the return value should be 2.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       CountOnes(0) --> 0");
+            Console.WriteLine("       CountOnes(100) --> 3");
+            Console.WriteLine("       CountOnes(999) --> 8");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       The input will always be a valid integer(number).\n");
         }
     }
 }
