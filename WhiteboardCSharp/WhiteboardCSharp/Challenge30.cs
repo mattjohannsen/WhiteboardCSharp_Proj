e671f81c24a00cce9e69ffc08bfd37f226f5adbf
mattjohannsen <majohann@hotmail.com>
2020-07-07 22:06:21 -0500

    Added ChallengeDescription for Challenge 30 and solved challenge in one line with Linq

--- WhiteboardCSharp/WhiteboardCSharp/Challenge30.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge30.cs
@@ -10,17 +10,35 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 30");
-            Console.WriteLine(ReverseAndNot(123));
-            Console.WriteLine(ReverseAndNot(152));
-            Console.WriteLine(ReverseAndNot(123456789));
+            ChallengeDescription();
+            Console.WriteLine($"       ReverseAndNot(123) --> {ReverseAndNot(123)}");
+            Console.WriteLine($"       ReverseAndNot(152) --> {ReverseAndNot(152)}");
+            Console.WriteLine($"       ReverseAndNot(123456789) --> {ReverseAndNot(123456789)}");
         }
         public string ReverseAndNot(int i)
         {
-            string originalString = i.ToString();
-            string reverseString = new string(originalString.Reverse().ToArray());
-            string returnString = reverseString + originalString;
-            return returnString;
+            // Original Solution
+            //string originalString = i.ToString();
+            //string reverseString = new string(i.ToString().Reverse().ToArray());
+            //string returnString = reverseString + originalString;
+            // Bonus One Line Linq solution
+            return i.ToString() + new string(i.ToString().Reverse().ToArray());
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 30: ReverseAndNot");
+            Console.WriteLine("       Write a function that takes an integer i and returns a string with");
+            Console.WriteLine("       the integer backwards followed by the original integer.");
+            Console.WriteLine("       To illustrate:");
+            Console.WriteLine("       123");
+            Console.WriteLine("       We reverse 123 to get 321 and then add 123 to the end, resulting in 321123.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       ReverseAndNot(123) --> 321123");
+            Console.WriteLine("       ReverseAndNot(152) --> 251152");
+            Console.WriteLine("       ReverseAndNot(123456789) --> 987654321123456789");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       i is a non-negative integer.");
+            Console.WriteLine("       Bonus: By using System.Linq this should be completed in one line of code.\n");
         }
     }
 }
