65ff29682d0050ca66a1e34a8eab4f0cd01679eb
mattjohannsen <majohann@hotmail.com>
2020-07-13 21:57:42 -0500

    Added Challenge Description and condensed code for Challenge36

--- WhiteboardCSharp/WhiteboardCSharp/Challenge36.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge36.cs
@@ -10,31 +10,41 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge36");
-            Console.WriteLine(CharRepeatString("A4B5C2"));
-            Console.WriteLine(CharRepeatString("C2F1E5"));
-            Console.WriteLine(CharRepeatString("T4S2V2"));
-            Console.WriteLine(CharRepeatString("A1B2C3D4"));
+            ChallengeDescription();
+            Console.WriteLine($"       RepeatCharNoOfTimes('A4B5C2') --> {RepeatCharNoOfTimes("A4B5C2")}");
+            Console.WriteLine($"       RepeatCharNoOfTimes('C2F1E5') --> {RepeatCharNoOfTimes("C2F1E5")}");
+            Console.WriteLine($"       RepeatCharNoOfTimes('T4S2V2') --> {RepeatCharNoOfTimes("T4S2V2")}");
+            Console.WriteLine($"       RepeatCharNoOfTimes('A1B2C3D4') --> {RepeatCharNoOfTimes("A1B2C3D4")}");
 
         }
-        private string CharRepeatString(string inputString)
+        private string RepeatCharNoOfTimes(string inputString)
         {
             List<char> stringToList = inputString.ToList();
             string solutionString = "";
             for (int i = 0; i < stringToList.Count; i+=2)
             {
-                string charToAdd = stringToList[i].ToString();
-                int loopCount = Int32.Parse(stringToList[i + 1].ToString());
-                for (int j = 0; j < loopCount; j++)
+                for (int j = 0; j < Int32.Parse(stringToList[i + 1].ToString()); j++)
                 {
-                    solutionString = solutionString + charToAdd;
+                    solutionString = solutionString + stringToList[i].ToString();
                 }
             }
             return solutionString;
         }
+        private void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 35: Reverse Coding Challenge #1");
+            Console.WriteLine("       This is a reverse coding challenge.Normally you're given explicit directions with how");
+            Console.WriteLine("       to create a function. Here, you must generate your own function to to satisfy the");
+            Console.WriteLine("       relationship between the inputs and outputs.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       RepeatCharNoOfTimes(A4B5C2) --> AAAABBBBBCC");
+            Console.WriteLine("       RepeatCharNoOfTimes(C2F1E5) --> CCFEEEEE");
+            Console.WriteLine("       RepeatCharNoOfTimes(T4S2V2) --> TTTTSSVV");
+            Console.WriteLine("       RepeatCharNoOfTimes(A1B2C3D4) --> ABBCCCDDDD\n");
+        }
     }
 }
-//Reverse Coding Challenge #1
+//Reverse Coding Challenge #1 https://edabit.com/challenge/bqveyPRgcWZM7XzMQ
 //This is a reverse coding challenge.Normally you're given explicit directions with how to create a function. Here, you must generate your own function to satisfy the relationship between the inputs and outputs.
 //Your task is to create a function that, when fed the inputs below, produce the sample outputs shown.
 //Examples
