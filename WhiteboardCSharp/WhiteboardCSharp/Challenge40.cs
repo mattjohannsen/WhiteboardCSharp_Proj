c24f54e34efcd43aad7e6ddbe892b9b0271e2c11
mattjohannsen <majohann@hotmail.com>
2020-07-17 18:24:18 -0500

    Challenge 40. Added ChallengeDescription and condensed code

--- WhiteboardCSharp/WhiteboardCSharp/Challenge40.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge40.cs
@@ -11,22 +11,32 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 40");
-            Console.WriteLine(AlphabetIndex("Wow, does that work?"));
-            Console.WriteLine(AlphabetIndex("The river stole the gods."));
-            Console.WriteLine(AlphabetIndex("We have a lot of rain in June."));
+            ChallengeDescription();
+            Console.WriteLine($"       AlphabetIndex(Wow, does that work?) --> {AlphabetIndex("Wow, does that work?")}");
+            Console.WriteLine($"       AlphabetIndex(The river stole the gods.) --> {AlphabetIndex("The river stole the gods.")}");
+            Console.WriteLine($"       AlphabetIndex(We have a lot of rain in June.) --> {AlphabetIndex("We have a lot of rain in June.")}");
         }
         private string AlphabetIndex(string inputString)
         {
-            string nonLettersRemoved = Regex.Replace(inputString, "[^a-zA-Z0-9]", String.Empty);
-            List<char> stringAsList = nonLettersRemoved.ToList();
+            List<char> stringAsList = Regex.Replace(inputString, "[^a-zA-Z0-9]", String.Empty).ToList();
             List<int> solutionList = new List<int>();
             for (int i = 0; i < stringAsList.Count; i++)
             {
                 solutionList.Add(char.ToUpper(stringAsList[i]) - 64);
             }
-            string solutionString = string.Join(" ", solutionList);
-            return solutionString;
+            return string.Join(" ", solutionList);
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 40: Replace Letters With Position In Alphabet");
+            Console.WriteLine("       Create a function that takes a string and replaces each letter with its");
+            Console.WriteLine("       appropriate position in the alphabet. 'a' is 1, 'b' is 2, 'c' is 3, etc, etc.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       AlphabetIndex(Wow, does that work?) --> 23 15 23 4 15 5 19 20 8 1 20 23 15 18 11");
+            Console.WriteLine("       AlphabetIndex(The river stole the gods.) --> 20 8 5 18 9 22 5 18 19 20 15 12 5 20 8 5 7 15 4 19");
+            Console.WriteLine("       AlphabetIndex(We have a lot of rain in June.) --> 23 5 8 1 22 5 1 12 15 20 15 6 18 1 9 14 9 14 10 21 14 5");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       If any character in the string isn't a letter, ignore it.\n");
         }
     }
 }
