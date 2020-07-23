6e628a625e4e6df07a51b83f09f38b18db582a2f
mattjohannsen <majohann@hotmail.com>
2020-07-23 13:02:26 -0500

    Added ChallengeDescription for Challenge 44 and condensed code

--- WhiteboardCSharp/WhiteboardCSharp/Challenge44.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge44.cs
@@ -10,36 +10,37 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 44");
-            Console.WriteLine(ReverseSubstrationDifference(832));
-            Console.WriteLine(ReverseSubstrationDifference(51));
-            Console.WriteLine(ReverseSubstrationDifference(7977));
-            Console.WriteLine(ReverseSubstrationDifference(1));
-            Console.WriteLine(ReverseSubstrationDifference(665));
-            Console.WriteLine(ReverseSubstrationDifference(149));
+            ChallengeDescription();
+            Console.WriteLine($"       ReverseSubstrationDifference(832) --> {ReverseSubstrationDifference(832)}");
+            Console.WriteLine($"       ReverseSubstrationDifference(51) --> {ReverseSubstrationDifference(51)}");
+            Console.WriteLine($"       ReverseSubstrationDifference(7977) --> {ReverseSubstrationDifference(7977)}");
+            Console.WriteLine($"       ReverseSubstrationDifference(1) --> {ReverseSubstrationDifference(1)}");
+            Console.WriteLine($"       ReverseSubstrationDifference(665) --> {ReverseSubstrationDifference(665)}");
+            Console.WriteLine($"       ReverseSubstrationDifference(149) --> {ReverseSubstrationDifference(149)}");
         }
         public int ReverseSubstrationDifference(int inputNumber)
         {
-            int minuend = inputNumber;
-            int subrahend = ReverseInteger(inputNumber);
-            int difference = minuend - subrahend;
-            if (difference<=0)
-            {
-                return 0;
-            }
-            else
-            {
-                return difference;
-            }
+            return (inputNumber - ReverseInteger(inputNumber) >= 0) ? inputNumber - ReverseInteger(inputNumber) : 0;
         }
         public int ReverseInteger(int inputNumber)
         {
-            char[] charArray = inputNumber.ToString().ToCharArray();
-            Array.Reverse(charArray);
-            string arrayAsString = new string(charArray);
-            int stringAsInt = Int32.Parse(arrayAsString);
-            return stringAsInt;
+            return Int32.Parse(new string(inputNumber.ToString().Reverse().ToArray()));
 
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 44: Longest Common Ending");
+            Console.WriteLine("       This is a reverse coding challenge.Normally you're given explicit directions ");
+            Console.WriteLine("       with how to create a function. Here, you must generate your own function to ");
+            Console.WriteLine("       satisfy the relationship between the inputs and outputs.");
+            Console.WriteLine("       Your task is to create a function that, when fed the inputs below, produce the sample outputs shown.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       832 --> 594");
+            Console.WriteLine("       51 --> 36");
+            Console.WriteLine("       7977 --> 180");
+            Console.WriteLine("       1 --> 0");
+            Console.WriteLine("       665 --> 99");
+            Console.WriteLine("       149 --> 0\n");
         }
     }
 }
