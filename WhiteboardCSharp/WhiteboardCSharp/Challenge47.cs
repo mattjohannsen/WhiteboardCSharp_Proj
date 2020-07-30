0eba2fb1b7a1588766542c9f9646099c6d148156
mattjohannsen <majohann@hotmail.com>
2020-07-29 22:04:40 -0500

    Added ChallengeDescription for Challenge47 and condensed code

--- WhiteboardCSharp/WhiteboardCSharp/Challenge47.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge47.cs
@@ -10,24 +10,18 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 47");
-            Console.WriteLine(Challenge47.IsParselTongue("Sshe ssselects to eat that apple. "));
-            Console.WriteLine(Challenge47.IsParselTongue("She ssselects to eat that apple. "));
-            Console.WriteLine(Challenge47.IsParselTongue("Beatrice samples lemonade "));
-            Console.WriteLine(Challenge47.IsParselTongue("You ssseldom sssspeak sso boldly, ssso messmerizingly."));
+            ChallengeDescription();
+            Console.WriteLine($"       Sshe ssselects to eat that apple. --> {Challenge47.IsParselTongue("Sshe ssselects to eat that apple. ")}");
+            Console.WriteLine($"       She ssselects to eat that apple. --> {Challenge47.IsParselTongue("She ssselects to eat that apple. ")}");
+            Console.WriteLine($"       Beatrice samples lemonade --> {Challenge47.IsParselTongue("Beatrice samples lemonade ")}");
+            Console.WriteLine($"       You ssseldom sssspeak sso boldly, ssso messmerizingly. --> {Challenge47.IsParselTongue("You ssseldom sssspeak sso boldly, ssso messmerizingly.")}");
         }
-        public static bool IsParselTongue(string str)
+        public static bool IsParselTongue(string sentence)
         {
-            string[] arr = str.Split(' ');
-            char[][] wordsAsArrays = arr.Select(w => w.ToCharArray()).ToArray();
+            char[][] wordsAsArrays = sentence.Split(' ').Select(w => w.ToCharArray()).ToArray();
             for (int i = 0; i < wordsAsArrays.Length; i++)
             {
-                bool wordIsParsel = IsWordParsel(wordsAsArrays[i]);
-                if (wordIsParsel)
-                {
-                    continue;
-                }
-                else
+                if (!IsWordParsel(wordsAsArrays[i]))
                 {
                     return false;
                 }
@@ -37,36 +31,37 @@ namespace WhiteboardCSharp
         public static bool IsWordParsel(char[] arr)
         {
             int sCount = 0;
-            int ssCount = 0;
             for (int j = 0; j < arr.Length-1; j++)
             {
-                char firstChar = char.ToLower(arr[j]);
-                char secondChar = char.ToLower(arr[j+1]);
                 if (char.ToLower(arr[j]) == 's')
                 {
                     sCount++;
+                    if (char.ToLower(arr[j + 1]) == 's')
+                    {
+                        return true;
+                    }
                 }
-                if (firstChar == 's' && secondChar == 's')
-                {
-                    ssCount ++;
-                }
             }
-            if (sCount == 0)
-            {
-                return true;
-            }
-            else if (sCount == 1)
-            {
-                return false;
-            }
-            else if (ssCount >= 1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return (sCount == 0);
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 47: Parseltongue");
+            Console.WriteLine("       Hermione has come up with a precise formula for determining whether or not a phrase");
+            Console.WriteLine("       was ssspoken by a parssseltongue(a reference from the Harry Potter universe;");
+            Console.WriteLine("       the language of ssserpents and those who can converse with them");
+            Console.WriteLine("       Each word in a sssentence must contain either:");
+            Console.WriteLine("       At least 2 instances of the letter 's' (i.e.must be together ss), or...");
+            Console.WriteLine("       Zero instances of the letter 's'.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       IsParselTongue(Sshe ssselects to eat that apple. ) --> true");
+            Console.WriteLine("       IsParselTongue(She ssselects to eat that apple. ) --> false");
+            Console.WriteLine("       She only contains one 's'.");
+            Console.WriteLine("       IsParselTongue(Beatrice samples lemonade) --> false");
+            Console.WriteLine("       While 'samples' has 2 instances of 's', they are not together.");
+            Console.WriteLine("       IsParselTongue(You ssseldom sssspeak sso boldly, ssso messmerizingly.) --> true");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       There should be no words with only one instance of the letter 's' (see example #2).\n");
         }
     }
 }
