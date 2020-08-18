ebf930755f6455c84864369f61b48ef59f7678f7
mattjohannsen <majohann@hotmail.com>
2020-08-17 19:32:10 -0500

    Added Challenge Description for Challenge 61

--- WhiteboardCSharp/WhiteboardCSharp/Challenge61.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge61.cs
@@ -10,7 +10,7 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 61");
+            ChallengeDescription();
             Console.WriteLine($"       {ConvertTime("12:00 am")}");
             Console.WriteLine($"       {ConvertTime("6:20 pm")}");
             Console.WriteLine($"       {ConvertTime("21:00")}");
@@ -20,6 +20,19 @@ namespace WhiteboardCSharp
         {
             DateTime dt = DateTime.Parse(time);
             return (time.Contains("m")) ? dt.ToString("H:mm") : dt.ToString("h:mm tt").ToLower();
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 61: 12 vs 24 Hours");
+            Console.WriteLine("       Create a function that converts 12-hour time to 24-hour time or vice versa.Return the output as a string.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       ConvertTime(12:00 am) --> 0:00");
+            Console.WriteLine("       ConvertTime(6:20 pm) --> 18:20");
+            Console.WriteLine("       ConvertTime(21:00) --> 9:00 pm");
+            Console.WriteLine("       ConvertTime(5:05) --> 5:05 am");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       A 12-hour time input will be denoted with an am or pm suffix.");
+            Console.WriteLine("       A 24-hour input time contains no suffix.\n");
         }
     }
 }
