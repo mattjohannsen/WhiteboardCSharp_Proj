da6de22797c6ea9add281e2b916c34c898995323
mattjohannsen <majohann@hotmail.com>
2020-08-25 22:49:20 -0500

    Successfully sorted Array Ascending for Challenge 68

--- WhiteboardCSharp/WhiteboardCSharp/Challenge68.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge68.cs
@@ -11,6 +11,73 @@ namespace WhiteboardCSharp
         public void RunChallenge()
         {
             Console.WriteLine("       Challenge 68");
+            string[] inputArr = { "John Locke", "Thomas Aquinas", "David Hume", "Rene Descartes" };
+            string[] sortedArray = SortContacts(inputArr, "ASC");
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.WriteLine($"       {sortedArray[i]}");
+            }
+        }
+        public static string[] SortContacts(string[] names, string sort)
+        {
+            for (int i = 0; i < names.Length - 1; i++)
+            {
+                for (int j = 0; j < names.Length -1; j++)
+                {
+                    //string fullName = names[i];
+                    //string[] fullNameArray = fullName.Split(' ');
+                    //string lastNameA = fullNameArray[1];
+                    string lastNameA = names[j].Split(' ')[1];
+                    string lastNameB = names[j + 1].Split(' ')[1];
+                    int sortValue = string.Compare(lastNameA, lastNameB);
+                    string temp;
+                    if (sort == "ASC")
+                    {
+                        if (sortValue == 1)
+                        {
+                            temp = names[j + 1];
+                            names[j + 1] = names[j];
+                            names[j] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (sortValue == -1)
+                        {
+                            temp = names[j];
+                            names[j] = names[j + 1];
+                            names[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            return names;
         }
     }
 }
+//Contact List
+//Write a sorting function that takes in an array of names and sorts them by last name either alphabetically(ASC) or reverse-alphabetically(DESC).
+//SortContacts(new string[] {
+//  "John Locke",
+//  "Thomas Aquinas",
+//  "David Hume",
+//  "Rene Descartes"
+//}, "ASC") --> {
+//  "Thomas Aquinas",
+//  "Rene Descartes",
+//  "David Hume",
+//  "John Locke"
+//}
+//// Aquinas (A) < Descartes (D) < Hume (H) < Locke (L)
+//SortContacts(new string[] {
+//  "Paul Erdos",
+//  "Leonhard Euler",
+//  "Carl Gauss"
+//}, "DESC") --> {
+//  "Carl Gauss",
+//  "Leonhard Euler",
+//  "Paul Erdos"
+//}
+//// Gauss (G) > Erdos (ER) > Euler (EU)
+//SortContacts([], "DESC") --> {}
+//SortContacts(null, "DESC") --> {}
