2c5c09b9b2f1f642f079e157c694f87a956a694d
mattjohannsen <majohann@hotmail.com>
2020-08-30 21:26:17 -0500

    Added Challenge Description for Challenge 70

--- WhiteboardCSharp/WhiteboardCSharp/Challenge70.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge70.cs
@@ -10,10 +10,11 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 70");
-            //Console.WriteLine($"       {CanComplete("butl", "beautiful")}");
+            ChallengeDescription();
+            Console.WriteLine($"       {CanComplete("butl", "beautiful")}");
+            Console.WriteLine($"       {CanComplete("butlz", "beautiful")}");
+            Console.WriteLine($"       {CanComplete("tulb", "beautiful")}");
             Console.WriteLine($"       {CanComplete("bbutl", "beautiful")}");
-            //Console.WriteLine($"       {CanComplete("siing", "something")}");
         }
         public static bool CanComplete(string initial, string word)
         {
@@ -41,7 +42,25 @@ namespace WhiteboardCSharp
         }
         return true;
         }
-
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 70: Complete the Word");
+            Console.WriteLine("       An input string can be completed if additional letters can be added and no letters");
+            Console.WriteLine("       need to be taken away to match the word.Furthermore, the order of the letters in");
+            Console.WriteLine("       the input string must be the same as the order of letters in the final word.");
+            Console.WriteLine("       Create a function that, given an input string, determines if the word can be completed.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       CanComplete(\"butl\", \"beautiful\") --> true");
+            Console.WriteLine("       We can add \"ea\" between \"b\" and \"u\", and \"ifu\" between \"t\" and \"l\".");
+            Console.WriteLine("       CanComplete(\"butlz\", \"beautiful\") --> false");
+            Console.WriteLine("       \"z\" does not exist in the word beautiful.");
+            Console.WriteLine("       CanComplete(\"tulb\", \"beautiful\") --> false");
+            Console.WriteLine("       Although \"t\", \"u\", \"l\" and \"b\" all exist in \"beautiful\", they are incorrectly ordered.");
+            Console.WriteLine("       CanComplete(\"bbutl\", \"beautiful\") --> false");
+            Console.WriteLine("       Too many \"b\"s, beautiful has only 1.");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       Both string input and word will be lowercased.\n");
+        }
     }
 }
 //Complete the Word
@@ -52,13 +71,10 @@ namespace WhiteboardCSharp
 //Examples
 //CanComplete("butl", "beautiful") --> true
 //// We can add "ea" between "b" and "u", and "ifu" between "t" and "l".
-
 //CanComplete("butlz", "beautiful") --> false
 //// "z" does not exist in the word beautiful.
-
 //CanComplete("tulb", "beautiful") --> false
 //// Although "t", "u", "l" and "b" all exist in "beautiful", they are incorrectly ordered.
-
 //CanComplete("bbutl", "beautiful") --> false
 //// Too many "b"s, beautiful has only 1.
 //Notes
