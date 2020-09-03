35e425dad3522d38584d7cfeaf220bd666e9d78f
mattjohannsen <majohann@hotmail.com>
2020-09-02 22:56:04 -0500

    Added Challenge Description for Challenge 72

--- WhiteboardCSharp/WhiteboardCSharp/Challenge72.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge72.cs
@@ -10,9 +10,9 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 72");
-            Console.WriteLine($"        {CannotCapture(new int[,] { { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 1, 0, 1, 0 }, { 0, 1, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 1, 0, 0, 0 } })}");
-            Console.WriteLine($"        {CannotCapture(new int[,] { { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 0, 0, 1, 0, 1, 0, 1 }, { 1, 0, 0, 0, 1, 0, 1, 0 }, { 0, 0, 0, 0, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 1, 0, 0, 1, 0, 0, 0, 1 } })}");
+            ChallengeDescription();
+            Console.WriteLine($"       {CannotCapture(new int[,] { { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 1, 0, 1, 0 }, { 0, 1, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 1, 0, 0, 0 } })}");
+            Console.WriteLine($"       {CannotCapture(new int[,] { { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 0, 0, 1, 0, 1, 0, 1 }, { 1, 0, 0, 0, 1, 0, 1, 0 }, { 0, 0, 0, 0, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 1, 0, 0, 1, 0, 0, 0, 1 } })}");
         }
         public static bool CannotCapture(int[,] board)
         {
@@ -45,6 +45,15 @@ namespace WhiteboardCSharp
                 }
             }
             return true;
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 54: Knights on a Board");
+            Console.WriteLine("       Write a function that returns true if the knights are placed on a chessboard such that no ");
+            Console.WriteLine("       knight can capture another knight.Here, 0s represent empty squares and 1s represent knights.");
+            Console.WriteLine("       Notes");
+            Console.WriteLine("       Knights can be present in any of the 64 squares.");
+            Console.WriteLine("       No other pieces except knights exist.\n");
         }
     }
 }
