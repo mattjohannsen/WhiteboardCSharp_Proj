8a8f575525a5d7c8186cee84a02561e65368dd6f
mattjohannsen <majohann@hotmail.com>
2020-09-04 11:00:46 -0500

    Added ChallengeDescription for Challenge73: Least Common Multiple

--- WhiteboardCSharp/WhiteboardCSharp/Challenge73.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge73.cs
@@ -10,12 +10,15 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 73");
-            //int[] inputArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] inputArray = { 67, 34, 12, 3, 2 };
-            
-            Console.WriteLine(LCM(inputArray));
-            //Console.WriteLine(LCM(xxx));
+            ChallengeDescription();
+            int[] inputArray1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine($"       LCM([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]) --> {LCM(inputArray1)}");
+            int[] inputArray2 = { 5 };
+            Console.WriteLine($"       LCM([ 5 ]) --> {LCM(inputArray2)}");
+            int[] inputArray3 = { 5, 7, 11 };
+            Console.WriteLine($"       LCM([ 5, 7, 11 ]) --> {LCM(inputArray3)}");
+            int[] inputArray4 = { 5, 7, 11, 35, 55, 77 };
+            Console.WriteLine($"       LCM([ 5, 7, 11, 35, 55, 77 ]) --> {LCM(inputArray4)}\n");
         }
         public static int LCM(int[] nums)
         {
@@ -62,6 +65,17 @@ namespace WhiteboardCSharp
             {
                 return GetGCF(num2, num1 % num2);
             }
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 73: Least Common Multiple");
+            Console.WriteLine("       Given a array of integers, create a function that will find the smallest positive integer that is evenly");
+            Console.WriteLine("       divisible by all the members of the list.In other words, find the least common multiple(LCM).");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       LCM({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }) --> 2520");
+            Console.WriteLine("       LCM({ 5 }) --> 5");
+            Console.WriteLine("       LCM({ 5, 7, 11 }) --> 385");
+            Console.WriteLine("       LCM({ 5, 7, 11, 35, 55, 77 }) --> 385\n");
         }
     }
 }
