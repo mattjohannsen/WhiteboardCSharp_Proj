236d607c845046ca62920a16003b83c228c84905
mattjohannsen <majohann@hotmail.com>
2020-09-06 13:22:42 -0500

    Added ChallengeDescription for Challenge 75: Area of Overlapping Rectangles

--- WhiteboardCSharp/WhiteboardCSharp/Challenge75.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge75.cs
@@ -10,14 +10,27 @@ namespace WhiteboardCSharp
     {
         public void RunChallenge()
         {
-            Console.WriteLine("       Challenge 75");
+            ChallengeDescription();
             Console.WriteLine($"       {OverlappingRectangles(new int[] { 2, 1, 3, 4 }, new int[] { 3, 2, 2, 5 })}");
+            Console.WriteLine($"       {OverlappingRectangles(new int[] { 2, -9, 11, 5 }, new int[] { 5, -11, 2, 9 })}");
+            Console.WriteLine($"       {OverlappingRectangles(new int[] { -8, -7, 4, 7 }, new int[] { -5, -9, 4, 7 })}");
         }
         public static int OverlappingRectangles(int[] rect1, int[] rect2)
         {
             int xOverlap = Math.Min(rect1[0] + rect1[2], rect2[0] + rect2[2]) - Math.Max(rect1[0], rect2[0]);
             int yOverlap = Math.Min(rect1[1] + rect1[3], rect2[1] + rect2[3]) - Math.Max(rect1[1], rect2[1]);
             return ((xOverlap < 0) ? 0 : xOverlap) * ((yOverlap < 0) ? 0 : yOverlap);
+        }
+        public void ChallengeDescription()
+        {
+            Console.WriteLine("       Challenge 75: Area of Overlapping Rectangles");
+            Console.WriteLine("       Create a function that returns the area of the overlap between two rectangles.");
+            Console.WriteLine("       The function will receive two rectangles, each with the coordinates of the lower");
+            Console.WriteLine("       left corner followed by the width and the height int[] { x, y, width, height }.");
+            Console.WriteLine("       Examples");
+            Console.WriteLine("       OverlappingRectangles(new int[] { 2, 1, 3, 4 }, new int[] { 3, 2, 2, 5  }) --> 6");
+            Console.WriteLine("       OverlappingRectangles(new int[] { 2, -9, 11, 5 }, new int[] { 5, -11, 2, 9 }) --> 10");
+            Console.WriteLine("       OverlappingRectangles(new int[] { -8, -7, 4, 7 },  new int[] { -5, -9, 4, 7 }) --> 5\n");
         }
     }
 }
