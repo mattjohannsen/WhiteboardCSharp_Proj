77f7e1b471e582d7dc69ed665b0a2ebd28b2af63
mattjohannsen <majohann@hotmail.com>
2020-09-09 21:22:29 -0500

    Challenge77: Password Validation. Changed CheckPasswordLength method to validate using Regex

--- WhiteboardCSharp/WhiteboardCSharp/Challenge77.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge77.cs
@@ -18,124 +18,126 @@ namespace WhiteboardCSharp
             //Console.WriteLine(ValidatePassword("Pè7$areLove")); //false;
             //Console.WriteLine(ValidatePassword("azAZ09è")); //false;
         }
-        public static bool ValidatePassword(string password)
-        {
-            foreach (var character in password)
-            {                                                                                 //!@#$%^&*()+=_-{}[]:;\"'?<>,.
-                if (!Regex.IsMatch(character.ToString(), @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$"))
-                if (!Regex.IsMatch(character.ToString(), @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$"))
-                    //if (!Regex.IsMatch(character.ToString(), @"^(?=[^A - Z] *[A - Z])(?=[^a - z] *[a - z])(?=[^0 - 9] *[0 - 9])(?:([\w\d *? !:;])\1 ? (? !\1))+$(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$")) "
-
-                    {
-                    return false;
-                }
-            }
-            return true;
-            //^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(([a - z0 - 9])\2 ? (? !\2))+$(?=.{ 6,24}$)
-            
-            //return Regex.IsMatch(password, "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,24}$");
-        }
         //public static bool ValidatePassword(string password)
         //{
-        //    //"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"
-        //    if (!CheckPasswordLength(password))
-        //    {
-        //        return false;
-        //    }
-        //    if(!CheckForUpper(password))
-        //    {
-        //        return false;
-        //    }
-        //    if (!CheckForLower(password))
-        //    {
-        //        return false;
-        //    }
-        //    if (!CheckForNumber(password))
-        //    {
-        //        return false;
-        //    }
-        //    if (!CheckForTriples(password))
-        //    {
-        //        return false;
-        //    }
-        //    if (!AlphaNumericOrAllowed(password))
-        //    {
-        //        return false;
-        //    }
-        //    return true;
-        //}
-        //public static bool CheckPasswordLength(string password)
-        //{
-        //    return (password.Length >= 6 && password.Length <= 24);
-        //}
-        //public static bool CheckForUpper(string password)
-        //{
-        //    for (int i = 0; i < password.Length; i++)
-        //    {
-        //        if (Char.IsUpper(password[i]))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
-        //public static bool CheckForLower(string password)
-        //{
-        //    for (int i = 0; i < password.Length; i++)
-        //    {
-        //        if (Char.IsLower(password[i]))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
-        //public static bool CheckForNumber(string password)
-        //{
-        //    for (int i = 0; i < password.Length; i++)
-        //    {
-        //        if (Char.IsDigit(password[i]))
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
-        //public static bool CheckForTriples(string password)
-        //{
-        //    for (int i = 0; i < password.Length - 2; i++)
-        //    {
-        //        if (password[i] == password[i + 1] && password[i] == password[i + 2])
-        //        {
+        //    foreach (var character in password)
+        //    {                                                                                 //!@#$%^&*()+=_-{}[]:;\"'?<>,.
+        //        if (!Regex.IsMatch(character.ToString(), @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$"))
+        //        if (!Regex.IsMatch(character.ToString(), @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$"))
+        //            //if (!Regex.IsMatch(character.ToString(), @"^(?=[^A - Z] *[A - Z])(?=[^a - z] *[a - z])(?=[^0 - 9] *[0 - 9])(?:([\w\d *? !:;])\1 ? (? !\1))+$(?=.*[!@#$%^&*()+=_-{}[]:;""'?<>,.])[A-Za-z\d!@#$%^&*()+=_-{}[]:;""'?<>,.]{6,24}$")) "
+
+        //            {
         //            return false;
         //        }
         //    }
         //    return true;
+        //    //^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(([a - z0 - 9])\2 ? (? !\2))+$(?=.{ 6,24}$)
+
+        //    //return Regex.IsMatch(password, "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,24}$");
         //}
-        //public static bool AlphaNumericOrAllowed(string password)
-        //{
-        //    for (int i = 0; i < password.Length; i++)
-        //    {
-        //        char currentChar = password[i];
-        //        string allowed = "!@#$%^&*()+=_-{}[]:;\"'?<>,.";
-        //        if (Regex.IsMatch(currentChar.ToString(), "^[a-zA-Z0-9]*$")) 
-        //        {
-        //            continue;
-        //        }
-        //        else
-        //        {
-        //            if (allowed.Contains(password[i]))
-        //            {
-        //                continue;
-        //            }
-        //            else
-        //            {
-        //                return false;
-        //            }
-        //        }
-        //    }
-        //    return true;
-        //}
+        public static bool ValidatePassword(string password)
+        {
+            //"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"
+            if (!CheckPasswordLength(password))
+            {
+                return false;
+            }
+            if (!CheckForUpper(password))
+            {
+                return false;
+            }
+            if (!CheckForLower(password))
+            {
+                return false;
+            }
+            if (!CheckForNumber(password))
+            {
+                return false;
+            }
+            if (!CheckForTriples(password))
+            {
+                return false;
+            }
+            if (!AlphaNumericOrAllowed(password))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CheckPasswordLength(string password)
+        {
+            //return (password.Length >= 6 && password.Length <= 24);
+            Regex constraints = new Regex("^.{6,24}$");
+            return (constraints.IsMatch(password));
+        }
+        public static bool CheckForUpper(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsUpper(password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckForLower(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsLower(password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckForNumber(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsDigit(password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckForTriples(string password)
+        {
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                if (password[i] == password[i + 1] && password[i] == password[i + 2])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool AlphaNumericOrAllowed(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                char currentChar = password[i];
+                string allowed = "!@#$%^&*()+=_-{}[]:;\"'?<>,.";
+                if (Regex.IsMatch(currentChar.ToString(), "^[a-zA-Z0-9]*$"))
+                {
+                    continue;
+                }
+                else
+                {
+                    if (allowed.Contains(password[i]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
 //Password Validation
