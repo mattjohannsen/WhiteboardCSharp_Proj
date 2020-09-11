59b01098ce0bfac3adb458197ce3b85ccf79dd91
mattjohannsen <majohann@hotmail.com>
2020-09-10 19:05:01 -0500

    Condensed Code for Challenge78.

--- WhiteboardCSharp/WhiteboardCSharp/Challenge78.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge78.cs
@@ -11,46 +11,25 @@ namespace WhiteboardCSharp
         public void RunChallenge()
         {
             Console.WriteLine("       Challenge 78");
-            Console.WriteLine($"       (\"Hello, World!\", 2) --> {WrapAround("Hello, World!", 2)}");
+            //Console.WriteLine($"       (\"Hello, World!\", 2) --> {WrapAround("Hello, World!", 2)}");
             Console.WriteLine($"       (\"From what I gathered\", -4) --> {WrapAround("From what I gathered", -4)}");
-            Console.WriteLine($"       (\"S\", -60) --> {WrapAround("S", -60)}");
-            Console.WriteLine($"       (\"Nonscience\", -12) --> {WrapAround("Nonscience", -12)}");
-            Console.WriteLine($"       (\"Excelsior\", 30) --> {WrapAround("Excelsior", 30)}");
-            Console.WriteLine($"       (\"Incomprehensibilities\", 50) --> {WrapAround("Incomprehensibilities", 50)}");
+            //Console.WriteLine($"       (\"S\", -60) --> {WrapAround("S", -60)}");
+            //Console.WriteLine($"       (\"Nonscience\", -12) --> {WrapAround("Nonscience", -12)}");
+            //Console.WriteLine($"       (\"Excelsior\", 30) --> {WrapAround("Excelsior", 30)}");
+            //Console.WriteLine($"       (\"Incomprehensibilities\", 50) --> {WrapAround("Incomprehensibilities", 50)}");
         }
         public static string WrapAround(string input, int offset)
         {
-            if (input.Length == 1)
-            {
-                return input;
-            }
-            else if(System.Math.Abs(offset) > input.Length)
+            if (System.Math.Abs(offset) > input.Length || offset < 0)
             {
                 offset %= input.Length;
-                if (offset < 0)
-                {
-                    string firstString = input.Substring(input.Length + offset);
-                    string secondString = input.Substring(0, input.Length + offset);
-                    return firstString + secondString;
-                }
-                else
-                {
-                    string firstString = input.Substring(offset);
-                    string secondString = input.Substring(0, offset);
-                    return firstString + secondString;
-                }
-            }
-            else if (offset < 0)
-            {
-                string firstString = input.Substring(input.Length + offset);
-                string secondString = input.Substring(0, input.Length + offset);
-                return firstString + secondString;
+                return (offset < 0)
+                    ? input.Substring(input.Length + offset) + input.Substring(0, input.Length + offset)
+                    : input.Substring(offset) + input.Substring(0, offset);
             }
             else
             {
-                string firstString = input.Substring(offset);
-                string secondString = input.Substring(0, offset);
-                return firstString + secondString;
+                return input.Substring(offset) + input.Substring(0, offset);
             }
         }
     }
