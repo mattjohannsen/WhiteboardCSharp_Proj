43e0e29ee0ada32caa9eec083aeaa28283989062
mattjohannsen <majohann@hotmail.com>
2020-09-11 20:37:01 -0500

    Completed Challenge 79: Reverse the String(with a catch). All tests pass

--- WhiteboardCSharp/WhiteboardCSharp/Challenge79.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge79.cs
@@ -13,32 +13,131 @@ namespace WhiteboardCSharp
         {
             Console.WriteLine("       Challenge 79");
             Console.WriteLine(SpecialReverseString("Edabit"));
+            Console.WriteLine(SpecialReverseString("UPPER lower"));
+            Console.WriteLine(SpecialReverseString("1 23 456")); // 6 54 321
+            Console.WriteLine(SpecialReverseString("Hello World!"));
+            Console.WriteLine(SpecialReverseString("Where's your dog Daisy?"));
+            //Console.WriteLine(SpecialReverseString("addition(3, 2) = 5"));
         }
         public static string SpecialReverseString(string txt)
         {
             char[] arr = txt.ToCharArray();
-            char[] reversed = txt.Trim().ToCharArray().Reverse().ToArray();
+            string noSpaceString = txt.Replace(" ", String.Empty);
+            char[] noSpaceArray = noSpaceString.ToCharArray();
+            char[] reverseNoSpaceArray = noSpaceArray.Reverse().ToArray();
+            string reversedString = new string(reverseNoSpaceArray);
             for (int i = 0; i < arr.Length; i++)
             {
                 if (arr[i] == ' ')
                 {
-                    char temp = arr[i];
-                    reversed[i] = reversed[i + 1];
-                    reversed[i + 1] = temp;
-                }
-                else if (Regex.IsMatch(arr[i].ToString(), "^[a-zA-Z0-9]*$"))
-                {
-                    if (arr[i] == Char.ToUpper(arr[i]))
-                    {
-                        reversed[i] = Char.ToUpper(reversed[i]);
-                    }
-                    else
-                    {
-                        reversed[i] = Char.ToLower(reversed[i]);
-                    }
+                    reversedString = reversedString.Insert(i, " ");
                 }
             }
-            return new string(reversed);
+            char[] solution = reversedString.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Char.IsUpper(arr[i]) && Regex.IsMatch(arr[i].ToString(), "^[a-zA-Z]*$"))
+                {
+                    solution[i] = Char.ToUpper(solution[i]);
+                }
+                else
+                {
+                    solution[i] = Char.ToLower(solution[i]);
+                }
+            }
+            return reversedString;
+            //for (int i = arr.Length - 1; i > 0; i--)
+            //{
+            //    // reversed[i] is of type space
+            //    if (reversed[i] == ' ' && arr[i] != ' ')
+            //    {
+            //        char temp = (arr[i - 1] == Char.ToUpper(arr[i - 1])) ? Char.ToUpper(reversed[i - 1]) : Char.ToLower(reversed[i - 1]);
+            //        char behindChar = (arr[i - 1] == Char.ToUpper(arr[i - 1])) ? Char.ToUpper(reversed[i]) : Char.ToLower(reversed[i]);
+            //        reversed[i - 1] = behindChar;
+            //        reversed[i] = temp;
+            //    }
+            //    else if (Regex.IsMatch(arr[i].ToString(), "^[a-zA-Z0-9]*$"))
+            //    {
+            //        if (arr[i] == Char.ToUpper(arr[i]))
+            //        {
+            //            reversed[i] = Char.ToUpper(reversed[i]);
+            //        }
+            //        else
+            //        {
+            //            reversed[i] = Char.ToLower(reversed[i]);
+            //        }
+            //        if (i == 1)
+            //        {
+            //            reversed[i - 1] = (arr[i - 1] == Char.ToUpper(arr[i - 1])) ? Char.ToUpper(reversed[i -1]) : Char.ToLower(reversed[i - 1]);
+            //        }
+            //    }
+            //    else if (Regex.IsMatch(arr[i].ToString(), @"\W|_"))
+            //    {
+            //        reversed[i] = Char.ToLower(reversed[i]);
+            //    }
+            //    else
+            //    {
+            //        reversed[i] = Char.ToLower(reversed[i]);
+            //        if (i == 1)
+            //        {
+            //            if (Regex.IsMatch(reversed[i - 1].ToString(), "^[a-zA-Z0-9]*$"))
+            //            {
+            //                reversed[i - 1] = (arr[i - 1] == Char.ToUpper(arr[i - 1])) ? Char.ToUpper(reversed[i - 1]) : Char.ToLower(reversed[i - 1]);
+            //            }
+            //            else
+            //            {
+            //                reversed[i - 1] = Char.ToLower(reversed[i - 1]);
+            //            }
+
+            //        }
+            //    }
+            //}
+            //return new string(reversed);
+
+            //char[] arr = txt.ToCharArray();
+            //char[] reversed = txt.Trim().ToCharArray().Reverse().ToArray();
+            //for (int i = arr.Length -1; i > 0; i--)
+            //{
+            //if (reversed[i] == ' ' && arr[i] == ' ')
+            //{
+            //    continue;
+            //}
+            //else if (reversed[i] == ' ' && arr[i] != ' ')
+            //    {
+            //        char temp = reversed[i];
+            //        //(Regex.IsMatch(arr[i].ToString(), "^[a-zA-Z]*$")
+            //        reversed[i] = (arr[i - 1] == Char.ToUpper(arr[i - 1])) ? Char.ToUpper(reversed[i - 1]) : Char.ToLower(reversed[i - 1]);
+            //        reversed[i - 1] = (arr[i] == Char.ToUpper(arr[i])) ? reversed[i - 1] = Char.ToUpper(temp) : reversed[i - 1] = Char.ToLower(temp);
+            //        //if (arr[i - 1] == Char.ToUpper(arr[i - 1]))
+            //        //{
+            //        //    reversed[i] = Char.ToUpper(reversed[i - 1]);
+            //        //}
+            //        //else
+            //        //{
+            //        //    reversed[i] = Char.ToLower(reversed[i - 1]);
+            //        //}
+            //        if (arr[i] == Char.ToUpper(arr[i]))
+            //        {
+            //            reversed[i - 1] = Char.ToUpper(temp);
+            //        }
+            //        else
+            //        {
+            //            reversed[i - 1] = Char.ToLower(temp);
+            //        }
+            //    }
+            //    else if (Regex.IsMatch(arr[i].ToString(), "^[a-zA-Z]*$"))
+            //    {
+            //        if (arr[i] == Char.ToUpper(arr[i]))
+            //        {
+            //            reversed[i] = Char.ToUpper(reversed[i]);
+            //        }
+            //        else
+            //        {
+            //            reversed[i] = Char.ToLower(reversed[i]);
+            //        }
+            //    }
+            //}
+            //return new string(reversed);
         }
     }
 }
