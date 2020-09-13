dda0e24b56f81051b29a4913989bf8e33abc3e04
mattjohannsen <majohann@hotmail.com>
2020-09-13 12:38:43 -0500

    Completed Challenge 81: Find the Largest Numbers in a Group of Arrays

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge81.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge81
+    {
+        public static double[] FindLargest(double[][] values)
+        {
+            double[] solution = new double[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                double maxValue = values[i][0];
+                for (int j = 0; j < values[i].Length; j++)
+                {
+                    if (values[i][j] > maxValue)
+                    {
+                        maxValue = values[i][j];
+                    }
+                }
+                solution[i] = maxValue;
+            }
+            return solution;
+        }
+    }
+}
+//Find the Largest Numbers in a Group of Arrays https://edabit.com/challenge/nermqxzovZbfFBC9X
+//Create a function that takes an array of arrays with numbers.Return a new(single) array with the largest numbers of each.
+
+//Examples
+//FindLargest([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) --> [7, 90, 2]
+
+//FindLargest([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) --> [-34, -2, 7]
+
+//FindLargest([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) --> [0.7634, 9.32, 9]
+//Notes
+//Watch out for negative numbers.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge81_Test.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge81_Test
+    {
+        [TestMethod]
+        public void FindLargest_AllPositiveWholeNumbers_ReturnsAllPositiveWholeNumbers()
+        {
+            //Arrange
+            double[][] input = new double[][] { new double[] { 4, 2, 7, 1 }, new double[] { 20, 70, 40, 90 }, new double[] { 1, 2, 0 } };
+            double[] expected = new double[] { 7, 90, 2 };
+
+            //Act
+            double[] actual = Challenge81.FindLargest(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargest_MixedWholeNumbersAndDecimals_ReturnsWholeAndDecimalNumbers()
+        {
+            //Arrange
+            double[][] input = new double[][] { new double[] { 0.4321, 0.7634, 0.652 }, new double[] { 1.324, 9.32, 2.5423, 6.4314 }, new double[] { 9, 3, 6, 3 } };
+            double[] expected = new double[] { 0.7634, 9.32, 9 };
+
+            //Act
+            double[] actual = Challenge81.FindLargest(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargest_ContainsNegativeNumbers_ReturnsPositiveandNegativeNumbers()
+        {
+            //Arrange
+            double[][] input = new double[][] { new double[] { -34, -54, -74 }, new double[] { -32, -2, -65 }, new double[] { -54, 7, -43 } };
+            double[] expected = new double[] { -34, -2, 7 };
+
+            //Act
+            double[] actual = Challenge81.FindLargest(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargest_ContainsNegativesAndDecimals_ReturnsPositiveAndNegativeDecimalsAndWhole()
+        {
+            //Arrange
+            double[][] input = new double[][] { new double[] { 0.34, -5, 1.34 }, new double[] { -6.432, -1.762, -1.99 }, new double[] { 32, 65, -6 } };
+            double[] expected = new double[] { 1.34, -1.762, 65 };
+
+            //Act
+            double[] actual = Challenge81.FindLargest(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            double[][] input = new double[][] { new double[] { 0, 0, 0, 0 }, new double[] { 3, 3, 3, 3 }, new double[] { -2, -2 } };
+            double[] expected = new double[] { 0, 3, -2 };
+
+            //Act
+            double[] actual = Challenge81.FindLargest(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
