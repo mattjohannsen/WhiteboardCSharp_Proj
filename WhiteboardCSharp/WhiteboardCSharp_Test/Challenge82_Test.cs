5f8b4a4b5c0744cbcdce7e72a637c49a52fa09c2
mattjohannsen <majohann@hotmail.com>
2020-09-13 13:02:26 -0500

    Added Test Class for Challenge 82: Reverse the Case

---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge82_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge82_Test
+    {
+        [TestMethod]
+        public void ReverseCase_TwoWordsCapitalized_ReturnsInverseCase()
+        {
+            // Arrange
+            string input = "Happy Birthday";
+            string expected = "hAPPY bIRTHDAY";
+
+            // Act
+            string actual = Challenge82.ReverseCase(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
