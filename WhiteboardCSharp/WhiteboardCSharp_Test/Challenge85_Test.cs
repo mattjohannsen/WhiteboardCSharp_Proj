19db26b846f3df5d04c5997eab00635335c6ab8c
mattjohannsen <majohann@hotmail.com>
2020-09-16 09:44:25 -0500

    Added Unit Tests for Challenge85

---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge85_Test.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge85_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string txt = "There is a bomb.";
+            string expected = "Duck!!!";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        public void TestMethod2()
+        {
+            //Arrange
+            string txt = "Hey, did you find it?";
+            string expected = "Hey, did you find it?";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        public void TestMethod3()
+        {
+            //Arrange
+            string txt = "Hey, did you think there is a bomb?";
+            string expected = "Duck!!!";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        public void TestMethod4()
+        {
+            //Arrange
+            string txt = "Hey, did you think there is a bomb?";
+            string expected = "Hey, did you find it?";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        public void TestMethod5()
+        {
+            //Arrange
+            string txt = "Hey, did you find the BoMb?";
+            string expected = "Duck!!!";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        public void TestMethod6()
+        {
+            //Arrange
+            string txt = "Commotion in the third, a bomb is found!";
+            string expected = "Duck!!!";
+
+            //Act
+            string actual = Challenge85.Bomb(txt);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
