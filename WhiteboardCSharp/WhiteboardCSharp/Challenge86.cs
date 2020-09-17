8bd2c711b8bf538dcfba740c0f32e631d908df51
mattjohannsen <majohann@hotmail.com>
2020-09-17 11:10:36 -0500

    Completed Challenge86: Return the Middle Characters. All tests pass

--- WhiteboardCSharp/WhiteboardCSharp/Challenge86.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge86.cs
@@ -10,7 +10,30 @@ namespace WhiteboardCSharp
     {
         public static string GetMiddle(string str)
         {
-            return str;
+            char[] arr = str.ToCharArray();
+            string returnString = "";
+            if (arr.Length == 1)
+            {
+                return str;
+            }
+            else if (arr.Length % 2 == 0)
+            {
+                returnString += arr[arr.Length / 2 - 1].ToString() + arr[arr.Length / 2].ToString();
+            }
+            else
+            {
+                returnString += arr[arr.Length / 2];
+            }
+            return returnString;
         }
     }
 }
+//Return the Middle Character(s) of a String
+//Create a function that takes a string and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.
+//Examples
+//GetMiddle("test") --> "es"
+//GetMiddle("testing") --> "t"
+//GetMiddle("middle") --> "dd"
+//GetMiddle("A") --> "A"
+//Notes
+//All test cases contain a single word(as a string).
--- WhiteboardCSharp/WhiteboardCSharp_Test/Challenge86_Test.cs
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge86_Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
 
 namespace WhiteboardCSharp_Test
 {
@@ -9,6 +10,142 @@ namespace WhiteboardCSharp_Test
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            string str = "test";
+            string expected = "es";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "testing";
+            string expected = "t";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "middle";
+            string expected = "dd";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "A";
+            string expected = "A";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "inhabitant";
+            string expected = "bi";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "brown";
+            string expected = "o";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string str = "pawn";
+            string expected = "aw";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string str = "cabinet";
+            string expected = "i";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            string str = "fresh";
+            string expected = "e";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            string str = "shorts";
+            string expected = "or";
+
+            //Act
+            string actual = Challenge86.GetMiddle(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
+//[TestCase("test", Result = "es")]
+//[TestCase("testing", Result = "t")]
+//[TestCase("middle", Result = "dd")]
+//[TestCase("A", Result = "A")]
+//[TestCase("inhabitant", Result = "bi")]
+//[TestCase("brown", Result = "o")]
+//[TestCase("pawn", Result = "aw")]
+//[TestCase("cabinet", Result = "i")]
+//[TestCase("fresh", Result = "e")]
+//[TestCase("shorts", Result = "or")] 
