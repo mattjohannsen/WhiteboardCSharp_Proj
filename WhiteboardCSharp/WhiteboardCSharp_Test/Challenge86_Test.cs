7f9a1d533676d01eed3a6a25a0bf3246a4b6aa00
mattjohannsen <majohann@hotmail.com>
2020-09-16 20:39:54 -0500

    Added Challenge86.cs and Challenge86_Test

---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge86_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge86_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
