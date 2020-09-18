8fdcc1d9fd17c83efaf3b73fa14721f52c32cd2a
mattjohannsen <majohann@hotmail.com>
2020-09-17 21:24:03 -0500

    Added Challenge87_Test Unit Test

---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge87_Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge87_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int a = 0;
+            int b = 3;
+            int expected = 6;
+
+            //Act
+            int actual = Challenge87.BlockPlayer(a, b);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int a = 0;
+            int b = 8;
+            int expected = 4;
+
+            //Act
+            int actual = Challenge87.BlockPlayer(a, b);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int a = 4;
+            int b = 8;
+            int expected = 0;
+
+            //Act
+            int actual = Challenge87.BlockPlayer(a, b);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int a = 2;
+            int b = 5;
+            int expected = 8;
+
+            //Act
+            int actual = Challenge87.BlockPlayer(a, b);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int a = 1;
+            int b = 7;
+            int expected = 4;
+
+            //Act
+            int actual = Challenge87.BlockPlayer(a, b);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
