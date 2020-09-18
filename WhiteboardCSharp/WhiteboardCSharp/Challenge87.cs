e4155721a63d9cf2746efb54b40445f15cb93f80
mattjohannsen <majohann@hotmail.com>
2020-09-17 21:18:12 -0500

    Added Challenge87.cs: Block the square in Tic Tac Toe

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge87.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge87
+    {
+        public static int BlockPlayer(int a, int b)
+        {
+            return 1;
+        }
+    }
+}
