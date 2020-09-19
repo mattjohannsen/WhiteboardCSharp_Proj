68321d05878e2a91276e2daf53d77a5b86d3d700
mattjohannsen <majohann@hotmail.com>
2020-09-19 08:50:32 -0500

    Added Challenge89.cs and Challenge89_Test.cs for Is the Number Symmetrical Challenge

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge89.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge89
+    {
+        public static bool IsSymmetrical(int num)
+        {
+            return false;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge89_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge89_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
