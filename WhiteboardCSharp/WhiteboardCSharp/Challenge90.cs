ab63cf0ac3c6e2a2beb5e53c18a85ab2cdb7e277
mattjohannsen <majohann@hotmail.com>
2020-09-20 11:38:43 -0500

    Completed Challenge90: Stack the Boxes

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge90.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge90
+    {
+        public static int StackBoxes(int n)
+        {
+            return Convert.ToInt32(Math.Pow(n, 2));
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge90_Test.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge90_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int n = 1;
+            int expected = 1;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int n = 2;
+            int expected = 4;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int n = 0;
+            int expected = 0;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int n = 5;
+            int expected = 25;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int n = 27;
+            int expected = 729;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int n = 196;
+            int expected = 38416;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int n = 512;
+            int expected = 262144;
+
+            //Act
+            int actual = Challenge90.StackBoxes(n);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
