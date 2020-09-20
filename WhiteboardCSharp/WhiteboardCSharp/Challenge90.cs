204819ce1f32813a5824cb1590e15ab8555049d7
mattjohannsen <majohann@hotmail.com>
2020-09-20 11:48:36 -0500

    Added Description. All tests pass

--- WhiteboardCSharp/WhiteboardCSharp/Challenge90.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge90.cs
@@ -14,3 +14,13 @@ namespace WhiteboardCSharp
         }
     }
 }
+//Stack the Boxes https://edabit.com/challenge/h2BGWkeBcoiW2tSXX
+//Here's an image of four models. Some of the cubes are hidden behind other cubes. Model one consists of one cube. Model two consists of four cubes, and so on...
+//Stack the Boxes
+//Write a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible.
+//Examples
+//StackBoxes(1) --> 1
+//StackBoxes(2) --> 4
+//StackBoxes(0) --> 0
+//Notes
+//Step n is a positive integer.
