b47045808735c63ecaa0b2ff832b9986eb1ccecb
mattjohannsen <majohann@hotmail.com>
2020-09-21 19:03:35 -0500

    Completed Challenge91: Maskify the String. All tests pass

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge91.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge91
+    {
+        public static string Maskify(string str)
+        {
+            if (str.Length <= 4) return str;
+            char[] arr = str.ToCharArray();
+            for (int i = 0; i < arr.Length - 4; i++)
+            {
+                arr[i] = '#';
+            }
+            return new string(arr);
+        }
+    }
+}
+//Maskify the String https://edabit.com/challenge/RFeBL2TzSf7mRMNJi
+//Usually when you sign up for an account to buy something, your credit card number, phone number or answer to a secret question is partially obscured in some way.Since someone could look over your shoulder, you don't want that shown on your screen. Hence, the website masks these strings.
+//Your task is to create a function that takes a string, transforms all but the last four characters into "#" and returns the new masked string.
+//Examples
+//Maskify("4556364607935616") --> "############5616"
+//Maskify("64607935616") --> "#######5616"
+//Maskify("1") --> "1"
+//Maskify("") --> ""
+//Notes
+//The maskify function must accept a string of any length.
+//An empty string should return an empty string (fourth example above).
+// Steps
+// if string.length is less than or equal to 4 characters then return str
+// else
+// turn str into arr
+// loop through length - 4 times
+// replace arr[i] with '#'
+// return arr to String()
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge91_Test.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge91_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "4556364607935616";
+            string expected = "############5616";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "64607935616";
+            string expected = "#######5616";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "1";
+            string expected = "1";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "";
+            string expected = "";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "tBy>L/cMe+?<j:6n;C~H";
+            string expected = "################;C~H";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "12";
+            string expected = "12";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string str = "8Ikhlf6yoxPOwi5cB014eWbRumj7vJ";
+            string expected = "##########################j7vJ";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string str = "123";
+            string expected = "123";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            string str = ")E$aCU=e\"_";
+            string expected = "######=e\"_";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            string str = "2673951408";
+            string expected = "######1408";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            //Arrange
+            string str = "1234";
+            string expected = "1234";
+
+            //Act
+            string actual = Challenge91.Maskify(str);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
