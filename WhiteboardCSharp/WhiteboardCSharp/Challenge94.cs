2768c1ec80400bed4f803f6d410ef8e1e32ee950
mattjohannsen <majohann@hotmail.com>
2020-09-24 10:30:52 -0500

    Completed Challenge94: Find the Amount of Potatoes. All tests pass

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge94.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge94
+    {
+        public static int Potatoes(string potato)
+        {
+            return Regex.Matches(potato, "potato").Count;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge94_Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge94_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "potato";
+            int expected = 1;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "potatopotatocherry";
+            int expected = 2;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "potatopotatopotatoorange";
+            int expected = 3;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "potatopotatobananapotatopotato";
+            int expected = 4;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "potatopotatomangopotatopotatopotato";
+            int expected = 5;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "potatocucumberpotatopotatopotatopotatopotato";
+            int expected = 6;
+            //Act
+            int actual = Challenge94.Potatoes(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
