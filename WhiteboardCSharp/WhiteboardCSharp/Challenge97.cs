e4fbc3454cba7d8d73c6cf603569ac1e8692509c
mattjohannsen <majohann@hotmail.com>
2020-09-27 10:32:11 -0500

    Completed Challenge 97: Check is a Number is Prime. All tests pass

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge97.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge97
+    {
+        public static bool IsPrime(int x)
+        {
+            if (x == 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
+//Create a function that returns true if a number is prime, and false otherwise.A prime number is any positive integer that is evenly divisible by only two divisors: 1 and itself.
+//The first ten prime numbers are:
+//2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+//Examples
+//isPrime(31) --> true
+//isPrime(18) --> false
+//isPrime(11) --> true
+//Notes
+//A prime number has no other factors except 1 and itself.
+//If a number is odd it is not divisible by an even number.
+//1 is not considered a prime number.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge97_Test.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge97_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int x = 31;
+            bool expected = true;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int x = 18;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int x = 11;
+            bool expected = true;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int x = 12;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int x = 1;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int x = 3;
+            bool expected = true;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int x = 48;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            int x = 10;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            int x = 5;
+            bool expected = true;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            int x = 77;
+            bool expected = false;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            //Arrange
+            int x = 7;
+            bool expected = true;
+            //Act
+            bool actual = Challenge97.IsPrime(x);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
