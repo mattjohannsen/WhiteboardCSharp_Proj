8ee2e5be23c6d20667d11d4ebc68e415132e9123
mattjohannsen <majohann@hotmail.com>
2020-09-29 22:35:53 -0500

    Completed Challenge99: Return the Sum of the Two Smallest Numbers. All tests pass.

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge99.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge99
+    {
+        public static int SumSmallest(int[] values)
+        {
+            Array.Sort(values);
+            int value1 = 0;
+            int value2 = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] >= 0)
+                {
+                    value1 = values[i];
+                    value2 = values[i + 1];
+                    break;
+                }
+            }
+            return value1 + value2;
+        }
+    }
+}
+//Return the Sum of the Two Smallest Numbers
+//Create a function that takes an array of numbers and returns the sum of the two lowest positive numbers.
+//Examples
+//SumSmallest([19, 5, 42, 2, 77]) --> 7
+//SumSmallest([10, 343445353, 3453445, 3453545353453]) --> 3453455
+//SumSmallest([2, 9, 6, -1]) --> 8
+//SumSmallest([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]) --> 563
+//SumSmallest([3683, 2902, 3951, -475, 1617, -2385]) --> 4519
+//Notes
+//Don't count negative numbers.
+//Floats and empty arrays will not be used in any of the test cases.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge99_Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge99_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int[] values = { 19, 5, 42, 2, 77 };
+            int expected = 7;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int[] values = { 10, 343445353, 3453445, 345354534 };
+            int expected = 3453455;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int[] values = { 2, 9, 6, -1 };
+            int expected = 8;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int[] values = { 879, 953, 694, -847, 342, 221, -91, -723, 791, -587 };
+            int expected = 563;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int[] values = { 3683, 2902, 3951, -475, 1617, -2385 };
+            int expected = 4519;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int[] values = { 280, 134, 108 };
+            int expected = 242;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int[] values = { 280, 134, 108, 1 };
+            int expected = 109;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            int[] values = { 321, 406, -176 };
+            int expected = 727;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            int[] values = { 1, 1, 1, 1 };
+            int expected = 2;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            int[] values = { -1, -1, 1, 1 };
+            int expected = 2;
+            //Act
+            int actual = Challenge99.SumSmallest(values);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
