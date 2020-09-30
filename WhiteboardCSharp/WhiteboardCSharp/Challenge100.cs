0beb3a88f926276e05cd76a9e20f2a11116b1985
mattjohannsen <majohann@hotmail.com>
2020-09-30 17:14:38 -0500

    Completed Challenge100: Is the Word an Isogram? All tests pass

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge100.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge100
+    {
+        public static bool IsIsogram(string str)
+        {
+            char[] arr = str.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Regex.Matches(str.ToLower(), arr[i].ToString().ToLower()).Count > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+//Is the Word an Isogram? https://edabit.com/challenge/aQWAAz6SiApZBA8A8
+//An isogram is a word that has no duplicate letters.Create a function that takes a string and returns either true or false depending on whether or not it's an "isogram".
+//Examples
+//IsIsogram("Algorism") --> true
+//IsIsogram("PasSword") --> false
+//// Not case sensitive.
+//IsIsogram("Consecutive") --> false
+//Notes
+//Ignore letter case (should not be case sensitive).
+//All test cases contain valid one word strings.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge100_Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge100_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "Algorism";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "PasSword";
+            bool expected = false;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "Dermatoglyphics";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "Cat";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "Filmography";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "Consecutive";
+            bool expected = false;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string str = "Bankruptcies";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string str = "Unforgivable";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            string str = "Unpredictably";
+            bool expected = true;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            string str = "Moose";
+            bool expected = false;
+            //Act
+            bool actual = Challenge100.IsIsogram(str);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
