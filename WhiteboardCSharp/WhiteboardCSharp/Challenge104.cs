3ff3d0d5eef0606b1f9769e8139a696b71f83cc0
mattjohannsen <majohann@hotmail.com>
2020-10-04 10:28:43 -0500

    Completed Challenge104: Greatest Common Denominator. All tests pass.

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge104.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge104
+    {
+        public static int GCD(int n1, int n2)
+        {
+            if (n1 == n2)
+            {
+                return n1;
+            }
+            int smaller = Math.Min(n1, n2);
+            int larger = Math.Max(n1, n2);
+            for (int i = smaller; i > 1; i--)
+            {
+                if (smaller % i == 0)
+                {
+                    if (larger % i == 0)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return 1;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge104_Test.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge104_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int n1 = 32;
+            int n2 = 8;
+            int expected = 8;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int n1 = 8;
+            int n2 = 12;
+            int expected = 4;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int n1 = 17;
+            int n2 = 13;
+            int expected = 1;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int n1 = 14;
+            int n2 = 7;
+            int expected = 7;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int n1 = 32;
+            int n2 = 16;
+            int expected = 16;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int n1 = 17;
+            int n2 = 100;
+            int expected = 1;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int n1 = 55;
+            int n2 = 20;
+            int expected = 5;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            int n1 = 55;
+            int n2 = 22;
+            int expected = 11;
+            //Act
+            int actual = Challenge104.GCD(n1, n2);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
