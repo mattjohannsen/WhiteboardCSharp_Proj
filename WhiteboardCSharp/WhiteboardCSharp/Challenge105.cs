caba56788bdc6842587743a1e30a477ffaf2159a
mattjohannsen <majohann@hotmail.com>
2020-10-04 10:58:48 -0500

    Added framework for Challenge105: Is the Input Factorial of an Integer?

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge105.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge105
+    {
+        public static bool IsFactorial(int n)
+        {
+            return true;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge105_Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge105_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int n = 6;
+            bool expected = true;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int n = 16;
+            bool expected = false;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int n = 24;
+            bool expected = true;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int n = 36;
+            bool expected = false;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int n = 120;
+            bool expected = true;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int n = 721;
+            bool expected = false;
+            //Act
+            bool actual = Challenge105.IsFactorial(n);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
