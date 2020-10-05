28c2f0ef001e8400d42cf1413be965d0d1e4c823
mattjohannsen <majohann@hotmail.com>
2020-10-05 18:22:07 -0500

    Completed Challenge106: Get the File Name

--- WhiteboardCSharp/WhiteboardCSharp/Challenge106.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge106.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WhiteboardCSharp
 {
@@ -10,7 +11,7 @@ namespace WhiteboardCSharp
     {
         public static string GetFilename(string path)
         {
-            return path;
+            return Path.GetFileName(path);
         }
     }
 }
