d7f6bf1dd4f894dac10cb9c871a3a9ae2e4be121
mattjohannsen <majohann@hotmail.com>
2020-10-07 10:20:44 -0500

    Added Challenge109.cs and Challenge109_Test.cs for unit testing

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge109.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge109
+    {
+        public static string FormatNum(int num)
+        {
+            return "string";
+        }
+    }
+}
+//Format Number with Comma(s) Separating Thousands
+//Create a function that takes a number as an argument and returns a string formatted to separate thousands.
+//Examples
+//FormatNum(1000) --> "1,000"
+//FormatNum(100000) --> "100,000"
+//FormatNum(20) --> "20"
+//Notes
+//You can expect a valid number for all test cases.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge109_Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge109_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int num = 1000;
+            string expected = "1,000";
+            //Act
+            string actual = Challenge109.FormatNum(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int num = 1000000;
+            string expected = "1,000,000";
+            //Act
+            string actual = Challenge109.FormatNum(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int num = 20;
+            string expected = "20";
+            //Act
+            string actual = Challenge109.FormatNum(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int num = 0;
+            string expected = "0";
+            //Act
+            string actual = Challenge109.FormatNum(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int num = 12948;
+            string expected = "12,948";
+            //Act
+            string actual = Challenge109.FormatNum(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
