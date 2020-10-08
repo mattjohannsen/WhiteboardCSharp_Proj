2ff440649b4837164bf85dce47d94a192be24301
mattjohannsen <majohann@hotmail.com>
2020-10-08 12:49:13 -0500

    Added framework for Challenge110: Mystery Function 6

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge110.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge110
+    {
+        public static int MysteryFunc(int num)
+        {
+            return 1;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge110_Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge110_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int num = 152;
+            int expected = 10;
+            //Act
+            int actual = Challenge110.MysteryFunc(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int num = 832;
+            int expected = 48;
+            //Act
+            int actual = Challenge110.MysteryFunc(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int num = 5511;
+            int expected = 25;
+            //Act
+            int actual = Challenge110.MysteryFunc(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int num = 19;
+            int expected = 9;
+            //Act
+            int actual = Challenge110.MysteryFunc(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int num = 133;
+            int expected = 9;
+            //Act
+            int actual = Challenge110.MysteryFunc(num);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
