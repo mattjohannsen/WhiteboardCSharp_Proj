a673d22e65aebc39f8b87219a6d8882095ebb634
mattjohannsen <majohann@hotmail.com>
2020-10-10 21:07:17 -0500

    Completed Challenge111: Get the Century.

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge111.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge111
+    {
+        public static string Century(int year)
+        {
+            int rounded = ((int)Math.Ceiling(year / 100.0));
+            string str = rounded.ToString();
+            if (rounded == 11 || rounded == 12 || rounded == 13)
+            {
+                return str + "th century";
+            }
+            else
+            {
+                switch (rounded % 10)
+                {
+                    case 1:
+                        return str + "st century";
+                    case 2:
+                        return str + "nd century";
+                    case 3:
+                        return str + "rd century";
+                    default:
+                        return str + "th century";
+                }
+            }
+        }
+    }
+}
+//Get the Century
+//Create a function that takes in a year and returns the correct century.
+
+//Examples
+//Century(1756) --> "18th century"
+//Century(1555) --> "16th century"
+//Century(1000) --> "10th century"
+//Century(1001) --> "11th century"
+//Century(2005) --> "21st century"
+//Notes
+//All years will be between 1000 and 2010.
+//The 11th century is between 1001 and 1100.
+//The 18th century is between 1701-1800.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge111_Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge111_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int year = 1756;
+            string expected = "18th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int year = 1555;
+            string expected = "16th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int year = 1000;
+            string expected = "10th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int year = 1001;
+            string expected = "11th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int year = 2005;
+            string expected = "21st century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int year = 1789;
+            string expected = "18th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int year = 1510;
+            string expected = "16th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            int year = 1615;
+            string expected = "17th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            int year = 2000;
+            string expected = "20th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            int year = 1997;
+            string expected = "20th century";
+            //Act
+            string actual = Challenge111.Century(year);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
