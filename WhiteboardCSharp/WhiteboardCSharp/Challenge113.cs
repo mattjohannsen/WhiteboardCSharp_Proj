7b6955beadaa7ccd31298bf2ed57d039ceebf6f9
mattjohannsen <majohann@hotmail.com>
2020-10-11 13:08:45 -0500

    Added framework for Challenge113: No Yelling

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge113.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge113
+    {
+        public static string NoYelling(string phrase)
+        {
+            return "";
+        }
+    }
+}
+//No Yelling
+//Create a function that transforms sentences ending with multiple question marks? or exclamation marks ! into a sentence only ending with one without changing punctuation in the middle of the sentences.
+//Examples
+//NoYelling("What went wrong?????????") --> "What went wrong?"
+//NoYelling("Oh my goodness!!!") --> "Oh my goodness!"
+//NoYelling("I just!!! can!!! not!!! believe!!! it!!!") --> "I just!!! can!!! not!!! believe!!! it!"
+//// Only change repeating punctuation at the end of the sentence.
+//NoYelling("Oh my goodness!") --> "Oh my goodness!"
+//// Do not change sentences where there exists only one or zero exclamation marks/question marks.
+//NoYelling("I just cannot believe it.") --> "I just cannot believe it."
+//Notes
+//Only change ending punctuation - keep the exclamation marks or question marks in the middle of the sentence the same(see third example).
+//Don't worry about mixed punctuation (no cases that end in something like ?!??!).
+//Keep sentences that do not have question/exclamation marks the same.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge113_Test.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge113_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string phrase = "What went wrong?????????";
+            string expected = "What went wrong?";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string phrase = "Oh my goodness!!!";
+            string expected = "Oh my goodness!";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string phrase = "WHAT!";
+            string expected = "WHAT!";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string phrase = "WHAT?";
+            string expected = "WHAT?";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string phrase = "Oh my goodness!?";
+            string expected = "Oh my goodness!?";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string phrase = "I just cannot believe it.";
+            string expected = "I just cannot believe it.";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string phrase = "I just!!! can!!! not!!! believe!!! it!!!";
+            string expected = "I just!!! can!!! not!!! believe!!! it!";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string phrase = "That's a ton!! of cheese!!!!";
+            string expected = "That's a ton!! of cheese!";
+            //Act
+            string actual = Challenge113.NoYelling(phrase);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
