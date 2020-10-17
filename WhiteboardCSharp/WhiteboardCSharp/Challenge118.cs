9b9494485f891f3c57eb349a32153befdfb8eb2e
mattjohannsen <majohann@hotmail.com>
2020-10-17 11:50:14 -0500

    Added framework for Challenge118: Find Nemo

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge118.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge118
+    {
+        public static string FindNemo(string sentence)
+        {
+            
+            return "";
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge118_Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge118_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string sentence = "I am Ne mo Nemo !";
+            string expected = "I found Nemo at 5!";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string sentence = "N e m o is NEMO NeMo Nemo !";
+            string expected = "I found Nemo at 8!";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string sentence = "I am Nemo's dad Nemo senior .";
+            string expected = "I found Nemo at 5!";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string sentence = "Oh, hello !";
+            string expected = "I can't find Nemo :(";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string sentence = "Is it Nemos, Nemona, Nemoor or Garfield?";
+            string expected = "I can't find Nemo :(";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string sentence = "Nemo is a clown fish, he has white and orange stripes. Nemo , come back!";
+            string expected = "I found Nemo at 1!";
+            //Act
+            string actual = Challenge118.FindNemo(sentence);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
