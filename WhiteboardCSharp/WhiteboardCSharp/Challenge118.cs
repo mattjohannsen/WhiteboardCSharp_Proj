069dd70e43de2dada44361fd0b6c7f761cf37744
mattjohannsen <majohann@hotmail.com>
2020-10-18 11:38:18 -0500

    Added alternative solution to Challenge118

--- WhiteboardCSharp/WhiteboardCSharp/Challenge118.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge118.cs
@@ -12,6 +12,9 @@ namespace WhiteboardCSharp
         {
             int index = Array.IndexOf(sentence.Split(' '), "Nemo");
             return (index < 0) ? "I can't find Nemo :(" : $"I found Nemo at {index + 1}!";
+            ////string[] arr = sentence.Split(' ');
+            ////return (arr.Contains("Nemo")) ? $"I found Nemo at {Array.IndexOf(arr, "Nemo") + 1}!" : "I can't find Nemo :(";
+            ////return (sentence.Split(' ').Contains("Nemo")) ? $"I found Nemo at {Array.IndexOf(sentence.Split(' '), "Nemo") + 1}!" : "I can't find Nemo :(";
         }
     }
 }
