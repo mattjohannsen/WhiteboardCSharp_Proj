25674b5cc95bc42d1bd94155cc3d974e09a94716
mattjohannsen <majohann@hotmail.com>
2020-10-19 19:31:44 -0500

    Completed Challenge119: Find the Prime Number

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge119.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge119
+    {
+        public static string IsPrime(int[] primes, int num)
+        {
+            int min = 0;
+            int max = primes.Length - 1;
+            string found = "no";
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (num == primes[mid])
+                {
+                    found = "yes";
+                    break;
+                }
+                else if (num < primes[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return found;
+        }
+    }
+}
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge119_Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge119_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            int num = 3;
+            string expected = "yes";
+            //Act
+            string actual = Challenge119.IsPrime(primes, num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            int num = 4;
+            string expected = "no";
+            //Act
+            string actual = Challenge119.IsPrime(primes, num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            int num = 67;
+            string expected = "yes";
+            //Act
+            string actual = Challenge119.IsPrime(primes, num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            int num = 36;
+            string expected = "no";
+            //Act
+            string actual = Challenge119.IsPrime(primes, num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
