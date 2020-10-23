714f02f0d5b287088684fb05cf6b728d33f12e83
mattjohannsen <majohann@hotmail.com>
2020-10-23 14:59:39 -0500

    Completed Challenge123: Basic Email Validation. All unit tests pass

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge123.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge123
+    {
+        public static bool ValidateEmail(string str)
+        {
+            return (str.Contains("@") && str.Contains(".") && str.IndexOf("@") > 0 && str.LastIndexOf(".") > str.IndexOf("@"));
+        }
+    }
+}
+//Basic E-Mail Validation
+//Create a function that accepts a string, checks if it's a valid email address and returns either true or false, depending on the evaluation.
+
+//The string must contain an @ character.
+//The string must contain a.character.
+//The @ must have at least one character in front of it.
+//e.g. "e@edabit.com" is valid while "@edabit.com" is invalid.
+//The.and the @ must be in the appropriate places.
+//e.g. "hello.email@com" is invalid while "john.smith@email.com" is valid.
+//If the string passes these tests, it's considered a valid email address.
+
+//Examples
+//ValidateEmail("@gmail.com") --> false
+//ValidateEmail("hello.gmail@com") --> false
+//ValidateEmail("gmail") --> false
+//ValidateEmail("hello@gmail") --> false
+//ValidateEmail("hello@edabit.com") --> true
+//Notes
+//Check the Tests tab to see exactly what's being evaluated.
+//You can solve this challenge with RegEx, but it's intended to be solved with logic.
+//Solutions using RegEx will be accepted but frowned upon :(
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge123_Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge123_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "@edabit.com";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "@edabit";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "matt@edabit.com";
+            bool expected = true;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "hello.gmail@com";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "bill.gates@microsoft.com";
+            bool expected = true;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string str = "hello@email";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string str = "%^%$#%^%";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            string str = "www.email.com";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            string str = "email";
+            bool expected = false;
+            //Act
+            bool actual = Challenge123.ValidateEmail(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
