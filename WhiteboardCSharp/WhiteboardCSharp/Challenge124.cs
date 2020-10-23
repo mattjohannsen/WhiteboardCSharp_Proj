1fca1ee0bdc56caed41db65bd258a6234813b2d4
mattjohannsen <majohann@hotmail.com>
2020-10-23 16:50:21 -0500

    Completed Challenge124: Capitalize first letter of sentence

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge124.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge124
+    {
+        public static string MakeTitle(string str)
+        {
+            return Regex.Replace(str, @"(^\w)|(\s\w)", w => w.Value.ToUpper());
+        }
+    }
+}
+//Capitalize the First Letter of Each Word
+//Create a function that takes a string as an argument and converts the first character of each word to uppercase.Return the newly formatted string.
+
+//Examples
+//MakeTitle("This is a title") ➞ "This Is A Title"
+
+//MakeTitle("capitalize every word") ➞ "Capitalize Every Word"
+
+//MakeTitle("I Like Pizza") ➞ "I Like Pizza"
+
+//MakeTitle("PIZZA PIZZA PIZZA") ➞ "PIZZA PIZZA PIZZA"
+//Notes
+//You can expect a valid string for each test case.
+//Some words may contain more than one uppercase letter(see example #4).
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge124_Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge124_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "I am a title";
+            string expected = "I Am A Title";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "I AM A TITLE";
+            string expected = "I AM A TITLE";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "i aM a tITLE";
+            string expected = "I AM A TITLE";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "the first letter of every word is capitalized";
+            string expected = "The First Letter Of Every Word Is Capitalized";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "I Like Pizza";
+            string expected = "I Like Pizza";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            string str = "Don't count your ChiCKens BeFore They HatCh";
+            string expected = "Don't Count Your ChiCKens BeFore They HatCh";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            string str = "All generalizations are false, including this one";
+            string expected = "All Generalizations Are False, Including This One";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            //Arrange
+            string str = "Me and my wife lived happily for twenty years and then we met.";
+            string expected = "Me And My Wife Lived Happily For Twenty Years And Then We Met.";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            //Arrange
+            string str = "There are no stupid questions, just stupid people.";
+            string expected = "There Are No Stupid Questions, Just Stupid People.";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            //Arrange
+            string str = "1f you c4n r34d 7h15, you r34lly n33d 2 g37 l41d";
+            string expected = "1f You C4n R34d 7h15, You R34lly N33d 2 G37 L41d";
+            //Act
+            string actual = Challenge124.MakeTitle(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
