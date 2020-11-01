82434b4ae8a9b680f172010cd1641d71f324b862
mattjohannsen <majohann@hotmail.com>
2020-11-01 11:58:45 -0600

    Completed Challenge128: Scottish Screaming

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge128.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge128
+    {
+        public static string ToScottishScreaming(string str)
+        {
+            string allCaps = str.ToUpper();
+            var vowels = "AEIOU";
+            return string.Concat(allCaps.Select(c => vowels.Contains(c) ? 'E' : c));
+        }
+    }
+}
+//Scottish Screaming https://edabit.com/challenge/fcGARuJW6tcYyWMQw
+//A strong Scottish accent makes every vowel similar to an "e", so you should replace every vowel with an "e". Additionally, it is being screamed, so it should be in block capitals.
+
+//Create a function that takes a string and returns a string.
+
+//Examples
+//ToScottishScreaming("hello world") ➞ "HELLE WERLD"
+
+//ToScottishScreaming("Mr. Fox was very naughty") ➞ "MR. FEX WES VERY NEEGHTY"
+
+//ToScottishScreaming("Butterflies are beautiful!") ➞ "BETTERFLEES ERE BEEETEFEL!"
+//Notes
+//Make sure to include all punctuation that is in the original string.
+//You don't need any more namespaces than are already given.
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge128_Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge128_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string str = "lorem ipsum";
+            string expected = "LEREM EPSEM";
+            //Act
+            string actual = Challenge128.ToScottishScreaming(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string str = "Leeroy jenkins!";
+            string expected = "LEEREY JENKENS!";
+            //Act
+            string actual = Challenge128.ToScottishScreaming(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            string str = "A, wonderful, day, don't, you, think?";
+            string expected = "E, WENDERFEL, DEY, DEN'T, YEE, THENK?";
+            //Act
+            string actual = Challenge128.ToScottishScreaming(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            string str = "Hello world";
+            string expected = "HELLE WERLD";
+            //Act
+            string actual = Challenge128.ToScottishScreaming(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            string str = "start queueing you oafs";
+            string expected = "STERT QEEEEENG YEE EEFS";
+            //Act
+            string actual = Challenge128.ToScottishScreaming(str);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
