44579b2be81270649a2bbcc5f45f1e3d8d8829ea
mattjohannsen <majohann@hotmail.com>
2020-11-13 20:51:19 -0600

    Condensed code for Challenge130: The Major Sum

--- WhiteboardCSharp/WhiteboardCSharp/Challenge130.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge130.cs
@@ -10,43 +10,10 @@ namespace WhiteboardCSharp
     {
         public static int MajorSum(int[] arr)
         {
-            int positiveSum = 0;
-            int negativeSum = 0;
-            int zeroCount = 0;
-            List<int> solution = new List<int>();
-            bool hasPositives = false;
-            bool hasNegatives = false;
-            bool hasZeros = false;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] > 0)
-                {
-                    positiveSum += arr[i];
-                    hasPositives = true;
-                }
-                else if (arr[i] < 0)
-                {
-                    negativeSum += arr[i];
-                    hasNegatives = true;
-                }
-                else
-                {
-                    zeroCount++;
-                    hasZeros = true;
-                }
-            }
-            if (hasPositives) solution.Add(positiveSum);
-            if (hasNegatives) solution.Add(negativeSum);
-            if (hasZeros) solution.Add(zeroCount);
-            int largest = solution.Max();
-            if (Math.Abs(negativeSum) > largest)
-            {
-                return negativeSum;
-            }
-            else
-            {
-                return solution.Max();
-            }
+            var positiveSum = arr.Where(x => x > 0).Sum();
+            var negativeSum = arr.Where(x => x < 0).Sum();
+            var zeroCount = arr.Where(x => x == 0).Count();
+            return (Math.Abs(negativeSum) > Math.Max(zeroCount, positiveSum)) ? negativeSum : Math.Max(positiveSum, zeroCount);
         }
     }
 }
