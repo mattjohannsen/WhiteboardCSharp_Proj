f4d94c610cc81ceee76af1c4452635b61e5d5496
mattjohannsen <majohann@hotmail.com>
2020-11-13 20:48:36 -0600

    Completed Challenge130.cs: The Major Sum

--- WhiteboardCSharp/WhiteboardCSharp/Challenge130.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge130.cs
@@ -10,7 +10,43 @@ namespace WhiteboardCSharp
     {
         public static int MajorSum(int[] arr)
         {
-            return 1;
+            int positiveSum = 0;
+            int negativeSum = 0;
+            int zeroCount = 0;
+            List<int> solution = new List<int>();
+            bool hasPositives = false;
+            bool hasNegatives = false;
+            bool hasZeros = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveSum += arr[i];
+                    hasPositives = true;
+                }
+                else if (arr[i] < 0)
+                {
+                    negativeSum += arr[i];
+                    hasNegatives = true;
+                }
+                else
+                {
+                    zeroCount++;
+                    hasZeros = true;
+                }
+            }
+            if (hasPositives) solution.Add(positiveSum);
+            if (hasNegatives) solution.Add(negativeSum);
+            if (hasZeros) solution.Add(zeroCount);
+            int largest = solution.Max();
+            if (Math.Abs(negativeSum) > largest)
+            {
+                return negativeSum;
+            }
+            else
+            {
+                return solution.Max();
+            }
         }
     }
 }
