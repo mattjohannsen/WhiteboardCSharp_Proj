51aac8a275adfae7d8a2f8b92fc46d3ecb62e4b4
mattjohannsen <majohann@hotmail.com>
2020-11-16 21:44:33 -0600

    Completed Challenge132: OddishOrEvenish

---
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge132.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardCSharp
+{
+    public class Challenge132
+    {
+        public static string OddishOrEvenish(int num)
+        {
+            //int[] arr = num.ToString().Select(n => Convert.ToInt32(n)).ToArray();
+            //int arrSum = arr.Sum();
+            //return (arrSum % 2 == 0) ? "Evenish" : "Oddish";
+            return (num.ToString().Select(n => Convert.ToInt32(n)).ToArray().Sum() % 2 == 0) ? "Evenish" : "Oddish";
+        }
+    }
+}
+//Oddish vs.Evenish
+//Create a function that determines whether a number is Oddish or Evenish.A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even.If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
+
+//For example, OddishOrEvenish(121) should return "Evenish", since 1 + 2 + 1 = 4. OddishOrEvenish(41) should return "Oddish", since 4 + 1 = 5.
+
+//Examples
+//OddishOrEvenish(43) ➞ "Oddish"
+//// 4 + 3 = 7
+//// 7 % 2 = 1
+
+//OddishOrEvenish(373) ➞ "Oddish"
+//// 3 + 7 + 3 = 13
+//// 13 % 2 = 1
+
+//OddishOrEvenish(4433) ➞ "Evenish"
+//// 4 + 4 + 3 + 3 = 14
+//// 14 % 2 = 0
---
+++ WhiteboardCSharp/WhiteboardCSharp_Test/Challenge132_Test.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteboardCSharp;
+
+namespace WhiteboardCSharp_Test
+{
+    [TestClass]
+    public class Challenge132_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int num = 43;
+            string expected = "Oddish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int num = 373;
+            string expected = "Oddish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            int num = 55551;
+            string expected = "Oddish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            int num = 694;
+            string expected = "Oddish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            int num = 4433;
+            string expected = "Evenish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Arrange
+            int num = 11;
+            string expected = "Evenish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //Arrange
+            int num = 211112;
+            string expected = "Evenish";
+            //Act
+            string actual = Challenge132.OddishOrEvenish(num);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
