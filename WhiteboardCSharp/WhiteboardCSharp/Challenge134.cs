c3b2239edc26f628139eea4a6852be302b136e48
mattjohannsen <majohann@hotmail.com>
2020-11-22 18:03:30 -0600

    Condensed code for Challenge134

--- WhiteboardCSharp/WhiteboardCSharp/Challenge134.cs
+++ WhiteboardCSharp/WhiteboardCSharp/Challenge134.cs
@@ -10,13 +10,10 @@ namespace WhiteboardCSharp
     {
         public static string FlipEndChars(object str)
         {
-            string strStr = str.ToString();
-            if (!(str is string) || strStr.Length < 2)
-            {
-                return "Incompatible.";
-            }
-            string switched = (new StringBuilder(strStr).Remove(strStr.Length - 1, 1).Remove(0, 1).Insert(0, strStr[strStr.Length - 1]).Insert(strStr.Length - 1, strStr[0])).ToString();
-            return (strStr[0] == strStr[strStr.Length - 1]) ? "Two's a pair." : switched;
+            string text = str.ToString();
+            if (!(str is string) || text.Length < 2) return "Incompatible.";
+            if (text[0] == text[text.Length - 1]) return "Two's a pair.";
+            return (new StringBuilder(text).Remove(text.Length - 1, 1).Remove(0, 1).Insert(0, text[text.Length - 1]).Insert(text.Length - 1, text[0])).ToString();
         }
     }
 }
